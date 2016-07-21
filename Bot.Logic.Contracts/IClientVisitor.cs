b60c955faf2f95ec9feb15f774d8bd60195ff342
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2016-07-20 21:24:25 -0500

    Created IClientVisitor, IBanReceived, IBroadcastReceived, IMuteReceived, IPrivateMessageReceived, IPublicMessageReceived, ISubonlyReceived, IUnMuteBanReceived. Added Bot.Models.Contracts project

---
+++ Bot.Logic.Contracts/IClientVisitor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bot.Models.Contracts;
+
+namespace Bot.Logic.Contracts {
+  public interface IClientVisitor {
+    void Visit(IPrivateMessageReceived privateMessageReceived);
+    void Visit(IPublicMessageReceived publicMessageReceived);
+    void Visit(IMuteReceived muteReceived);
+    void Visit(IUnMuteBanReceived unMuteBanReceived);
+    void Visit(ISubonlyReceived subonlyReceived);
+    void Visit(IBanReceived banReceived);
+    void Visit(IBroadcastReceived broadcastReceived);
+  }
+}
