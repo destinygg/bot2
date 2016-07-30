12eab7aff5090a650640d4b73f0cb6554d211b69
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2016-07-30 15:10:03 -0500

    Added Bot.Logic and Bot.Models

---
+++ Bot.Logic/MessageProcessor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bot.Logic.Contracts;
+using Bot.Models.Contracts;
+
+namespace Bot.Logic {
+  public class MessageProcessor : IMessageProcessor {
+
+    public void Process(IPrivateMessageReceived privateMessageReceived) {
+      
+    }
+
+    public void Process(IPublicMessageReceived publicMessageReceived) {
+      
+    }
+
+    public void Process(IMuteReceived muteReceived) {
+      
+    }
+
+    public void Process(IUnMuteBanReceived unMuteBanReceived) {
+      
+    }
+
+    public void Process(ISubonlyReceived subonlyReceived) {
+      
+    }
+
+    public void Process(IBanReceived banReceived) {
+      
+    }
+
+    public void Process(IBroadcastReceived broadcastReceived) {
+      
+    }
+  }
+}
---
+++ Bot.Models/PublicMessageReceived.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bot.Models.Contracts;
+
+namespace Bot.Models {
+  public class PublicMessageReceived : IPublicMessageReceived {
+    public DateTime Received { get; }
+    public IUser Sender { get; }
+    public string Text { get; set; }
+  }
+}
