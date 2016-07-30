daa4b54bcdbea0824b73f526d3c1c56dd3a236b4
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2016-07-30 15:11:34 -0500

    Added ISender

---
+++ Bot.Client.Contracts/ISender.cs
@@ -0,0 +1,12 @@
+﻿using Bot.Models.Contracts;
+
+namespace Bot.Client.Contracts {
+  public interface ISender {
+    void Send(IPublicMessageReceived publicMessageReceived);
+    void Send(IMuteReceived muteReceived);
+    void Send(IUnMuteBanReceived unMuteBanReceived);
+    void Send(ISubonlyReceived subonlyReceived);
+    void Send(IBanReceived banReceived);
+    void Send(IBroadcastReceived broadcastReceived);
+  }
+}
