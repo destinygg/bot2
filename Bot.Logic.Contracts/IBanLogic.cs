cf5e30a24df82a3958fa979b5ec9755a6688adc4
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2016-08-05 10:45:51 -0500

    Created IBanLogic

---
+++ Bot.Logic.Contracts/IBanLogic.cs
@@ -0,0 +1,8 @@
+﻿using Bot.Models.Contracts;
+using System.Collections.Generic;
+
+namespace Bot.Logic.Contracts {
+  public interface IBanLogic {
+    IEnumerable<IBroadcast> Process(IPublicMessageReceived publicMessageReceived);
+  }
+}
--- Bot.Logic/ReceivedProcessor.cs
+++ Bot.Logic/ReceivedProcessor.cs
@@ -10,7 +10,12 @@ using Bot.Models.Contracts;
 
 namespace Bot.Logic {
   public class ReceivedProcessor : IReceivedProcessor {
+    private readonly IBanLogic _banLogic;
     private readonly ISender _sender;
+
+    public ReceivedProcessor(IBanLogic banLogic) {
+      _banLogic = banLogic;
+    }
 
     public ReceivedProcessor(ISender sender) {
       _sender = sender;
@@ -23,6 +28,10 @@ namespace Bot.Logic {
     public void Process(IPublicMessageReceived publicMessageReceived) {
       var confirm = new PublicMessage($"Processor processing a public message: {publicMessageReceived.Text}");
       _sender.Send(confirm);
+      var outbox = _banLogic.Process(publicMessageReceived);
+      foreach (var outobject in outbox) {
+        _sender.Send(outobject);
+      }
     }
 
     public void Process(IMuteReceived muteReceived) {
