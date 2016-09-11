5958b749681cd9c99f508b3cfac69aff3f662800
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2016-09-11 16:28:32 -0500

    Created unit tests for all DB schema

---
+++ Bot.Database.Tests/StalkSchema.cs
@@ -0,0 +1,66 @@
+﻿using Bot.Database.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SQLite;
+
+namespace Bot.Database.Tests {
+  [TestClass]
+  public class StalkSchema {
+    private const string DbFileName = "Bot.sqlite";
+
+    [TestMethod]
+    public void StalkSchemaId() {
+      // Arrange
+      using (var db = new SQLiteConnection(DbFileName)) {
+        var tableInfo = db.GetTableInfo(nameof(Stalk));
+
+        // Act
+        var actual = tableInfo.Exists(x => x.Name == nameof(Stalk.Id));
+
+        // Assert
+        Assert.IsTrue(actual);
+      }
+    }
+
+    [TestMethod]
+    public void StalkHistorySchemaNick() {
+      // Arrange
+      using (var db = new SQLiteConnection(DbFileName)) {
+        var tableInfo = db.GetTableInfo(nameof(Stalk));
+
+        // Act
+        var actual = tableInfo.Exists(x => x.Name == nameof(Stalk.Nick));
+
+        // Assert
+        Assert.IsTrue(actual);
+      }
+    }
+
+    [TestMethod]
+    public void StalkSchemaRawTime() {
+      // Arrange
+      using (var db = new SQLiteConnection(DbFileName)) {
+        var tableInfo = db.GetTableInfo(nameof(Stalk));
+
+        // Act
+        var actual = tableInfo.Exists(x => x.Name == nameof(Stalk.Time));
+
+        // Assert
+        Assert.IsTrue(actual);
+      }
+    }
+
+    [TestMethod]
+    public void StalkSchemaRawText() {
+      // Arrange
+      using (var db = new SQLiteConnection(DbFileName)) {
+        var tableInfo = db.GetTableInfo(nameof(Stalk));
+
+        // Act
+        var actual = tableInfo.Exists(x => x.Name == nameof(Stalk.Text));
+
+        // Assert
+        Assert.IsTrue(actual);
+      }
+    }
+  }
+}
---
+++ Bot.Database.Tests/StateStringsSchema.cs
@@ -0,0 +1,38 @@
+﻿using Bot.Database.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SQLite;
+
+namespace Bot.Database.Tests {
+  [TestClass]
+  public class StateStringsSchema {
+    private const string DbFileName = "Bot.sqlite";
+
+    [TestMethod]
+    public void StateStringsSchemaKey() {
+      // Arrange
+      using (var db = new SQLiteConnection(DbFileName)) {
+        var tableInfo = db.GetTableInfo(nameof(StateStrings));
+
+        // Act
+        var actual = tableInfo.Exists(x => x.Name == nameof(StateStrings.Key));
+
+        // Assert
+        Assert.IsTrue(actual);
+      }
+    }
+
+    [TestMethod]
+    public void StateStringsSchemaValue() {
+      // Arrange
+      using (var db = new SQLiteConnection(DbFileName)) {
+        var tableInfo = db.GetTableInfo(nameof(StateStrings));
+
+        // Act
+        var actual = tableInfo.Exists(x => x.Name == nameof(StateStrings.Value));
+
+        // Assert
+        Assert.IsTrue(actual);
+      }
+    }
+  }
+}
