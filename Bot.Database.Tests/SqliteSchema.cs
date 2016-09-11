eb50ddeee5444946ac4ad2c3856730361782892e
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2016-09-11 16:09:06 -0500

    Added sqlite.testsettings, a test sqlite database, and a unit test JsonUserHistorySchema that passes

---
+++ Bot.Database.Tests/SqliteSchema.cs
@@ -0,0 +1,16 @@
+﻿using Bot.Database.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SQLite;
+
+namespace Bot.Database.Tests {
+  [TestClass]
+  public class SqliteSchema {
+    [TestMethod]
+    public void JsonUserHistorySchema() {
+      var db = new SQLiteConnection("Bot.sqlite");
+      var tableInfo = db.GetTableInfo(nameof(JsonUserHistory));
+      var actual = tableInfo.Exists(x => x.Name == "Id");
+      Assert.IsTrue(actual);
+    }
+  }
+}
