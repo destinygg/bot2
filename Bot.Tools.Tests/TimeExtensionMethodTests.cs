1ce6c9d74325dcde416bfee82a14f3e662cd6ba3
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2016-09-27 01:19:52 -0500

    Created TimeExtensionMethods class. Created Bot.Tools.Test project and tested ToUnixTime and FromUnixTime in TimeExtensionMethods.

---
+++ Bot.Tools.Tests/TimeExtensionMethodTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Bot.Tools;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bot.Tools.Tests {
+
+  [TestClass]
+  public class TimeExtensionMethodTests {
+
+    [TestMethod]
+    public void ToUnixTime_year2k() {
+      // Arrange
+      var year2K = new DateTime(2000, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+      var expected = 946684800L;
+
+      // Act
+      var actual = year2K.ToUnixTime();
+
+      // Assert
+      Assert.AreEqual(expected, actual);
+    }
+
+    [TestMethod]
+    public void FromUnixTime_year2k() {
+      // Arrange
+      var year2K = 946684800L;
+      var expected = new DateTime(2000, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+      // Act
+      var actual = year2K.FromUnixTime();
+
+      // Assert
+      Assert.AreEqual(expected, actual);
+    }
+
+  }
+}
---
+++ Bot.Tools/TimeExtensionMethods.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bot.Tools {
+  public static class TimeExtensionMethods {
+
+    // http://stackoverflow.com/a/7844741/625919
+    public static DateTime FromUnixTime(this long unixTime) {
+      var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+      return epoch.AddSeconds(unixTime);
+    }
+
+    // http://stackoverflow.com/a/7844741/625919
+    public static long ToUnixTime(this DateTime date) {
+      var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+      return Convert.ToInt64((date - epoch).TotalSeconds);
+    }
+
+  }
+}
