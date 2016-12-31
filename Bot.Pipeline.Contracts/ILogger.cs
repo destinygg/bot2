87e0bea54ff62ad0562e8d6e3b56ee4d6c11bee9
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2016-12-31 11:53:04 -0600

    Created ILogger and ConsoleLogger

---
+++ Bot.Pipeline.Contracts/ILogger.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bot.Pipeline.Contracts {
+  public interface ILogger {
+    void LogWarning(string warning);
+    void LogError(string error);
+    void LogInformation(string information);
+  }
+}
---
+++ Bot.Pipeline/ConsoleLogger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bot.Pipeline.Contracts;
+
+namespace Bot.Pipeline {
+  public class ConsoleLogger : ILogger {
+    public void LogWarning(string warning) {
+      Console.WriteLine(warning);
+    }
+
+    public void LogError(string error) {
+      Console.WriteLine(error);
+    }
+
+    public void LogInformation(string information) {
+      Console.WriteLine(information);
+    }
+  }
+}
