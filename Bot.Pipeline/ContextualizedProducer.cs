6d123d9dcd3911a949d9b176d6abcc53c4b445e7
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2016-12-31 20:32:55 -0600

    ContextualizedProducer now has a maximum capacity (of context) and uses a List

--- Bot.Pipeline/ContextualizedProducer.cs
+++ Bot.Pipeline/ContextualizedProducer.cs
@@ -4,11 +4,12 @@ using System.Threading.Tasks.Dataflow;
 using Bot.Models;
 using Bot.Models.Contracts;
 using Bot.Pipeline.Contracts;
+using Bot.Tools;
 
 namespace Bot.Pipeline {
   public class ContextualizedProducer : IContextualizedProducer {
     private readonly IReceivedProducer _receivedProducer;
-    private readonly Stack<IReceived> _allReceived = new Stack<IReceived>();
+    private readonly List<IReceived> _allReceived = new List<IReceived>(); // Todo: Optimization: Use a circular buffer
 
     public ContextualizedProducer(IReceivedProducer receivedProducer) {
       _receivedProducer = receivedProducer;
@@ -23,7 +24,10 @@ namespace Bot.Pipeline {
     }
 
     private IContextualized Transform(IReceived first) {
-      _allReceived.Push(first);
+      _allReceived.Insert(0, first);
+      if (_allReceived.Count > Settings.ContextSize) {
+        _allReceived.RemoveAt(Settings.ContextSize);
+      }
       return new Contextualized(_allReceived.ToList()); // The .ToList() creates a new instance; important!
     }
   }
