49b1c24ebcc09f2b03d5499c402820841805cef5
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-01-02 14:17:06 -0600

    Added Bot.Logic.Tests project

---
+++ Bot.Logic.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bot.Logic.Tests {
+  [TestClass]
+  public class UnitTest1 {
+    [TestMethod]
+    public void TestMethod1() {
+      var mc = new ModRegex();
+      var nuke = mc.Nuke;
+      Assert.IsTrue(nuke.IsMatch("!nuke this"));
+      var s = nuke.Match("!nuke this").Groups[1];
+    }
+  }
+}
