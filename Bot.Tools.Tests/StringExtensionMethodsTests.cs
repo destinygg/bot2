6f81c97e4239e2d56323a68d70e321a5dea185ea
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-01-05 17:16:42 -0600

    Added SimilarTo, a string extension method, and tested it

---
+++ Bot.Tools.Tests/StringExtensionMethodsTests.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bot.Tools.Tests {
+
+  [TestClass]
+  public class StringExtensionMethodsTests {
+
+    [TestMethod]
+    public void PefectMatch() {
+      // Arrange
+      var stringA = "The quick brown fox jumped over the lazy dog.";
+      var stringB = "The quick brown fox jumped over the lazy dog.";
+
+      // Act
+      var one = stringA.SimilarTo(stringB);
+
+      // Assert
+      Assert.AreEqual(1, one);
+    }
+
+    [TestMethod]
+    public void PartialMatch() {
+      // Arrange
+      var stringA = "The quick brown fox jumped";
+      var stringB = "The quick brown fox jumped over the lazy dog.";
+
+      // Act
+      var semi = stringA.SimilarTo(stringB);
+
+      // Assert
+      Assert.IsTrue(0 < semi && semi < 1);
+    }
+
+    [TestMethod]
+    public void ZeroMatch() {
+      // Arrange
+      var stringA = "";
+      var stringB = "The quick brown fox jumped over the lazy dog.";
+
+      // Act
+      var zero = stringA.SimilarTo(stringB);
+
+      // Assert
+      Assert.AreEqual(0, zero);
+    }
+
+  }
+}
