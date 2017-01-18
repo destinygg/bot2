1f3e0021795545e2ef39bc6de45c2ef4bf9d6eee
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-01-18 02:44:29 -0600

    UnMuteBan => Pardon, part 2, to keep file lineage

--- Bot.Models/SendablePardon.cs
+++ Bot.Models/SendablePardon.cs
@@ -2,7 +2,7 @@
 using Bot.Models.Contracts;
 
 namespace Bot.Models {
-  [DebuggerDisplay("Sending UnMuteBan targeting: {Target})")]
+  [DebuggerDisplay("Sending Pardon targeting: {Target})")]
   public class SendablePardon : Pardon, ISendable {
     public SendablePardon(IUser target) : base(target) { }
   }
