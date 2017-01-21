6792450ebb6a597f4dae25383c354c157ccb3b7c
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-01-21 01:44:02 -0600

    Created ISampleReceived, InfiniteSampleReceived, and SampleReceived

---
+++ Bot.Pipeline/InfiniteSampleReceived.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Bot.Models;
+using Bot.Models.Contracts;
+using Bot.Pipeline.Contracts;
+
+namespace Bot.Pipeline {
+  public class InfiniteSampleReceived : ISampleReceived {
+
+    public IEnumerable<IReceived> Receiveds {
+      get {
+        var rng = new Random();
+        while (true) {
+          yield return new PublicReceivedMessage("Hi!" + rng.Next(), DateTime.Now);
+        }
+      }
+    }
+  }
+}
---
+++ Bot.Pipeline/SampleReceived.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Bot.Models;
+using Bot.Models.Contracts;
+using Bot.Pipeline.Contracts;
+
+namespace Bot.Pipeline {
+  public class SampleReceived : ISampleReceived {
+    private readonly IReceivedFactory _factory;
+
+    public SampleReceived(IReceivedFactory factory) {
+      _factory = factory;
+    }
+
+    public IEnumerable<IReceived> Receiveds => new List<IReceived> {
+        _factory.ModPublicReceivedMessage("!long"),
+        _factory.PublicReceivedMessage("hi"),
+        _factory.PublicReceivedMessage("banplox"),
+        _factory.PublicReceivedMessage("!time"),
+        _factory.ModPublicReceivedMessage("!sing"),
+        _factory.ModPublicReceivedMessage("!long"),
+      };
+  }
+}
