66d9823a70bcf2cd874ed02dd30d38210b035561
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-01-21 16:08:23 -0600

    Made InfiniteSampleReceived use ITimeService

--- Bot.Pipeline/InfiniteSampleReceived.cs
+++ Bot.Pipeline/InfiniteSampleReceived.cs
@@ -3,15 +3,21 @@ using System.Collections.Generic;
 using Bot.Models;
 using Bot.Models.Contracts;
 using Bot.Pipeline.Contracts;
+using Bot.Tools.Contracts;
 
 namespace Bot.Pipeline {
   public class InfiniteSampleReceived : ISampleReceived {
+    private readonly ITimeService _timeService;
+
+    public InfiniteSampleReceived(ITimeService timeService) {
+      _timeService = timeService;
+    }
 
     public IEnumerable<IReceived> Receiveds {
       get {
         var rng = new Random();
         while (true) {
-          yield return new PublicReceivedMessage("Hi!" + rng.Next(), DateTime.Now);
+          yield return new PublicReceivedMessage("Hi!" + rng.Next(), _timeService.UtcNow);
         }
       }
     }
