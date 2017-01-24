61c5b78101199f8f25f0a7b4d1b90743b6236b49
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-01-23 22:29:05 -0600

    Simplified FromUnixTime and ToUnixTime

--- Bot.Tools/TimeExtensionMethods.cs
+++ Bot.Tools/TimeExtensionMethods.cs
@@ -5,18 +5,12 @@ namespace Bot.Tools {
   public static class TimeExtensionMethods {
 
     // http://stackoverflow.com/a/7844741/625919
-    public static DateTime FromUnixTime(this long unixTime) {
-      var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-      return epoch.AddSeconds(unixTime);
-    }
+    public static DateTime FromUnixTime(this long unixTime) => TimeService.UnixEpoch.AddSeconds(unixTime);
 
-    // http://stackoverflow.com/a/7844741/625919
-    public static long ToUnixTime(this DateTime date) {
-      var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-      return Convert.ToInt64((date - epoch).TotalSeconds);
-    }
+      // http://stackoverflow.com/a/7844741/625919
+    public static long ToUnixTime(this DateTime date) => Convert.ToInt64((date - TimeService.UnixEpoch).TotalSeconds);
 
-    public static bool IsWithin(this DateTime test, DateTime now, TimeSpan window) =>
+      public static bool IsWithin(this DateTime test, DateTime now, TimeSpan window) =>
       (test - now).Duration() <= window;
     
     public static string ToPretty(this TimeSpan span, ILogger logger) {
