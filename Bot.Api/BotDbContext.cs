dbe882ffa421902a82087c89161de13511d2c1dc
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-01-25 08:35:49 -0600

    Added Users, AutoPunishmentTypes, Autopunishments, and PunishedUsers to BotDbContext, along with the configurations  to make them many to many.

--- Bot.Api/BotDbContext.cs
+++ Bot.Api/BotDbContext.cs
@@ -7,10 +7,27 @@ namespace Bot.Api {
 
     #region DbSet
     public DbSet<StateInteger> StateIntegers { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<AutoPunishmentType> AutoPunishmentTypes { get; set; }
+    public DbSet<AutoPunishment> AutoPunishments { get; set; }
+    public DbSet<PunishedUser> PunishedUsers { get; set; }
     #endregion
 
     protected override void OnModelCreating(ModelBuilder modelBuilder) {
-      modelBuilder.Entity<StateInteger>(b => b.HasKey(si => new { si.Key }));
+      modelBuilder.Entity<StateInteger>().HasKey(si => si.Key);
+      modelBuilder.Entity<PunishedUser>()
+        .HasKey(pu => new { pu.UserId, pu.AutoPunishmentId });
+
+      //EFCore doesn't support many to many yet.
+      modelBuilder.Entity<PunishedUser>()
+          .HasOne(pt => pt.User)
+          .WithMany(p => p.PunishedUsers)
+          .HasForeignKey(pt => pt.UserId);
+
+      modelBuilder.Entity<PunishedUser>()
+          .HasOne(pt => pt.AutoPunishment)
+          .WithMany(t => t.PunishedUsers)
+          .HasForeignKey(pt => pt.AutoPunishmentId);
 
       base.OnModelCreating(modelBuilder);
     }
@@ -19,5 +36,6 @@ namespace Bot.Api {
       var sqliteConn = new SqliteConnection(@"DataSource = Bot.db");
       optionsBuilder.UseSqlite(sqliteConn);
     }
+
   }
 }
