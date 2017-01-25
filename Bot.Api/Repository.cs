7509fac64f030496916c077320ca7497a74770f8
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-01-25 08:54:21 -0600

    Created Repository and IRepository

---
+++ Bot.Api/Repository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using Bot.Database.Contracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bot.Api {
+  public class Repository<TEntity> : IRepository<TEntity> where TEntity : class {
+    protected readonly DbContext Context;
+    protected readonly DbSet<TEntity> Entities;
+
+    public Repository(DbContext context) {
+      Context = context;
+      Entities = Context.Set<TEntity>();
+    }
+
+    public IEnumerable<TEntity> GetAll() =>
+      Entities.ToList();
+
+    public IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate) =>
+      Entities.Where(predicate);
+
+    public TEntity SingleOrDefault(Expression<Func<TEntity, bool>> predicate) =>
+      Entities.SingleOrDefault(predicate);
+
+    public void Add(TEntity entity) =>
+      Entities.Add(entity);
+
+    public void Add(IEnumerable<TEntity> entities) =>
+      Entities.AddRange(entities);
+
+    public void Update(TEntity entity) =>
+      Entities.Update(entity);
+
+    public void Update(IEnumerable<TEntity> entities) =>
+      Entities.UpdateRange(entities);
+
+    public void Remove(TEntity entity) =>
+      Entities.Remove(entity);
+
+    public void Remove(IEnumerable<TEntity> entities) =>
+      Entities.RemoveRange(entities);
+  }
+}
---
+++ Bot.Database.Contracts/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Bot.Database.Contracts {
+  public interface IRepository<TEntity> where TEntity : class {
+    IEnumerable<TEntity> GetAll();
+    IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate);
+    TEntity SingleOrDefault(Expression<Func<TEntity, bool>> predicate);
+
+    void Add(TEntity entity);
+    void Add(IEnumerable<TEntity> entities);
+
+    void Update(TEntity entity);
+    void Update(IEnumerable<TEntity> entities);
+
+    void Remove(TEntity entity);
+    void Remove(IEnumerable<TEntity> entities);
+
+  }
+}
