79252b2dcb1c5bee6ddb2bbe741788b07f55ce41
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-01-25 02:08:50 -0600

    Created Bot.Database.Entities and moved StateInteger to it. Created AutoPunishment, User, PunishedUser, and AutoPunishmentType entities

---
+++ Bot.Database.Entities/Punishment.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace Bot.Database.Entities {
+
+  public class AutoPunishment {
+    public int Id { get; set; }
+    public string Term { get; set; }
+    public int Type { get; set; }
+    public long Duration { get; set; }
+    public virtual ICollection<PunishedUser> PunishedUsers { get; set; }
+  }
+
+  public class User {
+    public int Id { get; set; }
+    public string Nick { get; set; }
+    public virtual ICollection<PunishedUser> PunishedUsers { get; set; }
+  }
+
+  public class PunishedUser {
+    public int AutoPunishmentId { get; set; }
+    public virtual AutoPunishment AutoPunishment { get; set; }
+
+    public int UserId { get; set; }
+    public virtual User User { get; set; }
+
+    public int Count { get; set; }
+  }
+
+  public class AutoPunishmentType {
+    public int Id { get; set; }
+    public string Name { get; set; }
+  }
+  
+}
--- Bot.Database.Entities/StateInteger.cs
+++ Bot.Database.Entities/StateInteger.cs
@@ -1,13 +1,12 @@
-﻿namespace Bot.Api {
+﻿namespace Bot.Database.Entities {
   public class StateInteger {
     public StateInteger(string key, long value) {
       Key = key;
       Value = value;
     }
 
-    public StateInteger()
-    {
-      
+    public StateInteger() {
+
     }
 
     public string Key { get; set; }
