4550f34f7772d69d7d8ce3fc8f79f3115deca98e
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-01-26 00:02:28 -0600

    InitializeDb => DatabaseManager; it now contains EnsureCreated and EnsureDeleted

--- Bot.Api/DatabaseManager.cs
+++ Bot.Api/DatabaseManager.cs
@@ -2,14 +2,22 @@
 using Bot.Database.Entities;
 
 namespace Bot.Api {
-  public class InitializeDb {
-    public InitializeDb() {
+  public class DatabaseManager {
+
+    public void EnsureCreated() {
       var manager = new BotDbContextManager();
       manager.CallWithForeignKeysAndSaving(context => {
         context.Database.EnsureCreated();
         context.StateIntegers.Add(new StateInteger(nameof(IStateIntegerApi.LatestStreamOnTime), 0));
         //context.StateIntegers.Add(new StateInteger(nameof(IStateIntegerApi.LatestStreamOffTime), 0));
         //context.StateIntegers.Add(new StateInteger(nameof(IStateIntegerApi.DeathCount), 0));
+      });
+    }
+
+    public void EnsureDeleted() {
+      var manager = new BotDbContextManager();
+      manager.CallWithForeignKeysAndSaving(context => {
+        context.Database.EnsureDeleted();
       });
     }
 
