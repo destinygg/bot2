ec70ee081510a2a5af7557d8d834b01aad5f408e
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-01-26 00:03:16 -0600

    Added ReadWriteLatestStreamOnTime to StateIntegerApiTests

--- Bot.Api.Tests/StateIntegerApiTests.cs
+++ Bot.Api.Tests/StateIntegerApiTests.cs
@@ -4,21 +4,38 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Bot.Api.Tests {
   [TestClass]
   public class StateIntegerApiTests {
-    [TestMethod]
-    public void InitializeDb() {
-      new InitializeDb();
+
+
+    [TestInitialize]
+    public void Initialize() {
+      var manager = new DatabaseManager();
+      manager.EnsureCreated();
+    }
+
+    [TestCleanup]
+    public void Cleanup() {
+      var manager = new DatabaseManager();
+      manager.EnsureDeleted();
     }
 
     [TestMethod]
-    public void LatestLiveTimeUpdate() {
-      var s = new StateIntegerApi();
-      //s.LatestStreamOnTime = DateTime.UtcNow;
+    public void ReadWriteLatestStreamOnTime() {
+      var testWrite = DateTime.Now;
+
+      using (var context = new BotDbContext()) {
+        var stateIntegerApi = new StateIntegerApi(context.StateIntegers);
+        stateIntegerApi.LatestStreamOnTime = testWrite;
+        context.SaveChanges();
+      }
+
+      DateTime testRead;
+      using (var context = new BotDbContext()) {
+        var stateIntegerApi = new StateIntegerApi(context.StateIntegers);
+        testRead = stateIntegerApi.LatestStreamOnTime = testWrite;
+      }
+
+      Assert.AreEqual(testWrite, testRead);
     }
 
-    [TestMethod]
-    public void LatestLiveTimeRead() {
-      //var s = new StateIntegerApi();
-      //var time = s.LatestStreamOnTime;
-    }
   }
 }
