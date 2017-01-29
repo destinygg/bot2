27db7c4711e1aad9edf231e8c909c78c663422ea
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-01-29 10:59:32 -0600

    ConsoleSender now differentiates on type of message sent

--- Bot.Pipeline/ConsoleSender.cs
+++ Bot.Pipeline/ConsoleSender.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Bot.Models;
 using Bot.Models.Contracts;
 using Bot.Pipeline.Contracts;
 
@@ -7,7 +8,12 @@ namespace Bot.Pipeline {
   public class ConsoleSender : ISender {
     public void Send(IEnumerable<ISendable> sendables) {
       foreach (var sendable in sendables) {
-        Console.WriteLine(sendable);
+        if (sendable is SendablePublicMessage) {
+          var pm = (SendablePublicMessage) sendable;
+          Console.WriteLine($"Sending: {pm.Text}");
+        } else {
+          Console.WriteLine(sendable);
+        }
       }
     }
 
