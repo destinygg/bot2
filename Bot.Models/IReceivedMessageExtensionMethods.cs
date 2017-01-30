4037a37a918196d3087448c63ef131af8931784a
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-01-30 05:41:16 -0600

    Part 2 of previous commit to preserve file lineage Updating IReceivedMessageExtensionMethods

--- Bot.Models/IReceivedMessageExtensionMethods.cs
+++ Bot.Models/IReceivedMessageExtensionMethods.cs
@@ -1,9 +1,10 @@
 ﻿using System.Text.RegularExpressions;
+using Bot.Models.Contracts;
 
-namespace Bot.Models.Contracts {
-  public static class IMessageExtensionMethods {
-    public static bool StartsWith(this IMessage message, string phrase) => message.Text.StartsWith(phrase);
-    public static bool IsMatch(this IMessage message, Regex regex) => regex.IsMatch(message.Text);
+namespace Bot.Models {
+  public static class IReceivedMessageExtensionMethods {
+    public static bool StartsWith(this IReceivedMessage<IUser> message, string phrase) => message.Text.StartsWith(phrase);
+    public static bool IsMatch(this IReceivedMessage<IUser> message, Regex regex) => regex.IsMatch(message.Text);
 
   }
 }
