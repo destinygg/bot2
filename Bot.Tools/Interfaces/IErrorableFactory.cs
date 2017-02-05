9e00fe52f4a90c8caebc112291f4a406352a8cb4
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-02-05 15:03:41 -0600

    Created IErrorableFactory

---
+++ Bot.Tools/Interfaces/IErrorableFactory.cs
@@ -0,0 +1,10 @@
+﻿namespace Bot.Tools.Interfaces {
+  public interface IErrorableFactory<in T, out TResult> : IFactory<T, TResult> {
+
+    /// <summary>
+    /// When the Factory's .Create() encounters a problem, this will be the fallback Create.
+    /// This should return a TResult signifying an error or no action.
+    /// </summary>
+    TResult OnErrorCreate { get; }
+  }
+}
