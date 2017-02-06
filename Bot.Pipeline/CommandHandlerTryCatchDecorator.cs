61841380416de7de54dcd21420102fe1177a60e9
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-02-05 22:38:53 -0600

    Created ICommandHandler and CommandHandlerTryCatchDecorator

---
+++ Bot.Pipeline/CommandHandlerTryCatchDecorator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Bot.Pipeline.Interfaces;
+using Bot.Tools.Interfaces;
+
+namespace Bot.Pipeline {
+  public class CommandHandlerTryCatchDecorator<TCommand> : ICommandHandler<TCommand> {
+    private readonly ICommandHandler<TCommand> _decoratedHandler;
+    private readonly ILogger _logger;
+
+    public CommandHandlerTryCatchDecorator(ICommandHandler<TCommand> decoratedHandler, ILogger logger) {
+      _decoratedHandler = decoratedHandler;
+      _logger = logger;
+      _logger.LogInformation($"{nameof(CommandHandlerTryCatchDecorator<object>)} now decorates {_decoratedHandler.GetType()}");
+    }
+
+    public void Handle(TCommand command) {
+      try {
+        _decoratedHandler.Handle(command);
+      } catch (Exception e) {
+        _logger.LogError(e, $"Error occured in {nameof(CommandHandlerTryCatchDecorator<object>)} when handling {_decoratedHandler.GetType()}");
+      }
+    }
+
+  }
+}
---
+++ Bot.Pipeline/Interfaces/ICommandHandler.cs
@@ -0,0 +1,5 @@
+﻿namespace Bot.Pipeline.Interfaces {
+  public interface ICommandHandler<in TCommand> {
+    void Handle(TCommand command);
+  }
+}
