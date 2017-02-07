edff6695970fccb6aad25b0a32edc9b85827062d
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-02-07 03:32:15 -0600

    StateIntegerApiTests => StateIntegerRepositoryTests

--- Bot.Database.Tests/StateIntegerRepositoryTests.cs
+++ Bot.Database.Tests/StateIntegerRepositoryTests.cs
@@ -3,7 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Bot.Database.Tests {
   [TestClass]
-  public class StateIntegerApiTests {
+  public class StateIntegerRepositoryTests {
 
 
     [TestInitialize]
@@ -23,15 +23,15 @@ namespace Bot.Database.Tests {
       var testWrite = DateTime.Now;
 
       using (var context = new BotDbContext()) {
-        var stateIntegerApi = new StateIntegerRepository(context.StateIntegers);
-        stateIntegerApi.LatestStreamOnTime = testWrite;
+        var stateIntegerRepository = new StateIntegerRepository(context.StateIntegers);
+        stateIntegerRepository.LatestStreamOnTime = testWrite;
         context.SaveChanges();
       }
 
       DateTime testRead;
       using (var context = new BotDbContext()) {
-        var stateIntegerApi = new StateIntegerRepository(context.StateIntegers);
-        testRead = stateIntegerApi.LatestStreamOnTime = testWrite;
+        var stateIntegerRepository = new StateIntegerRepository(context.StateIntegers);
+        testRead = stateIntegerRepository.LatestStreamOnTime = testWrite;
       }
 
       Assert.AreEqual(testWrite, testRead);
@@ -42,15 +42,15 @@ namespace Bot.Database.Tests {
       var testWrite = DateTime.Now;
 
       using (var context = new BotDbContext()) {
-        var stateIntegerApi = new StateIntegerRepository(context.StateIntegers);
-        stateIntegerApi.LatestStreamOffTime = testWrite;
+        var stateIntegerRepository = new StateIntegerRepository(context.StateIntegers);
+        stateIntegerRepository.LatestStreamOffTime = testWrite;
         context.SaveChanges();
       }
 
       DateTime testRead;
       using (var context = new BotDbContext()) {
-        var stateIntegerApi = new StateIntegerRepository(context.StateIntegers);
-        testRead = stateIntegerApi.LatestStreamOffTime = testWrite;
+        var stateIntegerRepository = new StateIntegerRepository(context.StateIntegers);
+        testRead = stateIntegerRepository.LatestStreamOffTime = testWrite;
       }
 
       Assert.AreEqual(testWrite, testRead);
@@ -61,15 +61,15 @@ namespace Bot.Database.Tests {
       var testWrite = new Random().Next();
 
       using (var context = new BotDbContext()) {
-        var stateIntegerApi = new StateIntegerRepository(context.StateIntegers);
-        stateIntegerApi.DeathCount = testWrite;
+        var stateIntegerRepository = new StateIntegerRepository(context.StateIntegers);
+        stateIntegerRepository.DeathCount = testWrite;
         context.SaveChanges();
       }
 
       long testRead;
       using (var context = new BotDbContext()) {
-        var stateIntegerApi = new StateIntegerRepository(context.StateIntegers);
-        testRead = stateIntegerApi.DeathCount = testWrite;
+        var stateIntegerRepository = new StateIntegerRepository(context.StateIntegers);
+        testRead = stateIntegerRepository.DeathCount = testWrite;
       }
 
       Assert.AreEqual(testWrite, testRead);
