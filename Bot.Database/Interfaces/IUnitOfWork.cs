fa803656eea6deb69fa52678382091e3fa4adbb1
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-02-08 00:28:48 -0600

    Created IUnitOfWork and UnitOfWork

---
+++ Bot.Database/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Bot.Database.Interfaces {
+  public interface IUnitOfWork : IDisposable {
+    IStateIntegerRepository StateIntegers { get; }
+    IAutoPunishmentRepository AutoPunishments { get; }
+    IUserRepository Users { get; }
+    IPunishedUserRepository PunishedUsers { get; }
+    int Complete();
+  }
+}
---
+++ Bot.Database/UnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using Bot.Database.Interfaces;
+
+namespace Bot.Database {
+  public class UnitOfWork : IUnitOfWork {
+    private readonly BotDbContext _context;
+
+    public UnitOfWork(BotDbContext context) {
+      _context = context;
+      StateIntegers = new StateIntegerRepository(_context.StateIntegers);
+      AutoPunishments = new AutoPunishmentRepository(_context.AutoPunishments);
+      Users = new UserRepository(_context.Users);
+      PunishedUsers = new PunishedUserRepository(_context.PunishedUsers);
+    }
+
+    public IStateIntegerRepository StateIntegers { get; }
+    public IAutoPunishmentRepository AutoPunishments { get; }
+    public IUserRepository Users { get; }
+    public IPunishedUserRepository PunishedUsers { get; }
+    public int Complete() => _context.SaveChanges();
+
+    public void Dispose() {
+      _context.Dispose();
+    }
+
+  }
+}
