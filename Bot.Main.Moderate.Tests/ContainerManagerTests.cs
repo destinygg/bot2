fb2d85e18c740e9ba62dae47edadf046bd487e07
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-02-09 02:08:05 -0600

    Created a new project, Bot.Main.Moderate.Tests, and created ContainerManagerTests which contains the test VerifyContainer

---
+++ Bot.Main.Moderate.Tests/ContainerManagerTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bot.Main.Moderate.Tests {
+  [TestClass]
+  public class ContainerManagerTests {
+
+    [TestMethod]
+    public void VerifyContainer() {
+      // Arrange
+      // Act
+      var containerManager = new ContainerManager();
+
+      // Assert
+      Assert.IsNotNull(containerManager);
+    }
+
+  }
+}
