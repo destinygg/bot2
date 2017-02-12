86863030715a67c323068985cf72a6d3fbee2b03
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-02-12 12:41:59 -0600

    Added a test to verify that the DbContext is disposed.

--- Bot.Database.Tests/ScopedDatabaseServiceDecoratorTests.cs
+++ Bot.Database.Tests/ScopedDatabaseServiceDecoratorTests.cs
@@ -1,4 +1,5 @@
-﻿using Bot.Database.Interfaces;
+﻿using System;
+using Bot.Database.Interfaces;
 using Bot.Tests;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SimpleInjector;
@@ -17,14 +18,14 @@ namespace Bot.Database.Tests {
     }
 
     [TestMethod]
-    public void CreatingBotDbContext_BeforeExecutionContextScopeExists_ThrowsException() {
+    public void CreatingBotDbContext_BeforeExecutionContextScopeCreated_ThrowsException() {
       var exception = TestHelper.AssertCatch<ActivationException>(() => _container.GetInstance<IBotDbContext>());
 
       Assert.AreEqual($"The {nameof(IBotDbContext)} is registered as 'Execution Context Scope' lifestyle, but the instance is requested outside the context of a Execution Context Scope.", exception.Message);
     }
 
     [TestMethod]
-    public void CreatingBotDbContext_WithinExecutionContextScopeExists_DoesNotThrowException() {
+    public void CreatingBotDbContext_WithinExecutionContextScope_DoesNotThrowException() {
       // Should be automatically decorated with ScopedDatabaseServiceDecorator and therefore in an Execution scope.
       _container.GetInstance<IDatabaseService<IBotDbContext>>();
     }
@@ -36,6 +37,16 @@ namespace Bot.Database.Tests {
       var exception = TestHelper.AssertCatch<ActivationException>(() => _container.GetInstance<IBotDbContext>());
 
       Assert.AreEqual($"The {nameof(IBotDbContext)} is registered as 'Execution Context Scope' lifestyle, but the instance is requested outside the context of a Execution Context Scope.", exception.Message);
+    }
+
+    [TestMethod]
+    public void AccessingDbContext_AfterScopeCreatedAndDisposed_ThrowsDisposedException() {
+      var databaseService = _container.GetInstance<IDatabaseService<IBotDbContext>>();
+      var botDbContext = databaseService.Query(db => db);
+
+      var exception = TestHelper.AssertCatch<ObjectDisposedException>(() => botDbContext.AutoPunishments.Find(0));
+
+      Assert.AreEqual($"Cannot access a disposed object. A common cause of this error is disposing a context that was resolved from dependency injection and then later trying to use the same context instance elsewhere in your application. This may occur if you are calling Dispose() on the context, or wrapping the context in a using statement. If you are using dependency injection, you should let the dependency injection container take care of disposing context instances.\r\nObject name: '{nameof(BotDbContext)}'.", exception.Message);
     }
 
   }
