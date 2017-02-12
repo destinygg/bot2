a9ba1377f1bd5d5283268e2695f87c1e8cabc285
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-02-11 20:31:19 -0600

    Deleted WrapWithList and added Apply to ObjectExtensionMethods

--- Bot.Tools/ObjectExtensionMethods.cs
+++ Bot.Tools/ObjectExtensionMethods.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Bot.Tools {
   public static class ObjectExtensionMethods {
@@ -12,6 +13,21 @@ namespace Bot.Tools {
       yield return source;
     }
 
-    public static IList<T> WrapWithList<T>(this T source) => new List<T> { source };
+    /// <summary>
+    /// Applies a function to a value. Useful for method chaining in fluent syntax scenarios.
+    /// </summary>
+    /// <typeparam name="TInput">The value to apply to the <paramref name="function"/>.</typeparam>
+    /// <param name="source">The value to be applied.</param>
+    /// <param name="function">A function to be applied to the <paramref name="source"/>.</param>
+    /// <returns>A value of type <typeparamref name="TResult"/>; the result of applying <paramref name="function"/> to <paramref name="source"/>.</returns>
+    public static TResult Apply<TInput, TResult>(this TInput source, Func<TInput, TResult> function) => function(source);
+
+    /// <summary>
+    /// Applies an action to a value. Useful for method chaining in fluent syntax scenarios.
+    /// </summary>
+    /// <typeparam name="TInput">The value to apply to the <paramref name="action"/>.</typeparam>
+    /// <param name="source">The value to be applied.</param>
+    /// <param name="action">An action to be applied to the <paramref name="source"/>.</param>
+    public static void Apply<TInput>(this TInput source, Action<TInput> action) => action(source);
   }
 }
