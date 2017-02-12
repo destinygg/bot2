29a62e2a4e248a8104a97b40ebfcc7a231b6a6e7
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-02-12 01:04:18 -0600

    BotDbContextmanager now uses DatabaseService

--- Bot.Database/BotDbContextManager.cs
+++ Bot.Database/BotDbContextManager.cs
@@ -1,15 +1,22 @@
 ﻿using System;
 using Bot.Database.Interfaces;
+using Bot.Tools;
 using Microsoft.EntityFrameworkCore;
 
 namespace Bot.Database {
   public class BotDbContextManager {
+    private readonly DatabaseService<IBotDbContext> _databaseService;
+
+    public BotDbContextManager() {
+      var delegatedProvider = new DelegatedProvider<BotDbContext>(() => new BotDbContext());
+      _databaseService = new DatabaseService<IBotDbContext>(delegatedProvider);
+    }
 
     public void Save(Action<IBotDbContext> dbCommand) {
-      using (var context = new BotDbContext()) {
-        _InvokeWithEnforcedForeignKeys(dbCommand, context);
-        context.SaveChanges();
-      }
+      _databaseService.Command(db => {
+        _InvokeWithEnforcedForeignKeys(dbCommand, db);
+        db.SaveChanges();
+      });
     }
 
     private void _InvokeWithEnforcedForeignKeys(Action<IBotDbContext> dbCommand, IBotDbContext context) {
