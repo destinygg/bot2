77aa63c63c56dbbdfe608d9e6d271a4d6fd9ee76
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-02-11 20:36:48 -0600

    Added IProvider and DelegatedProvider to Bot.Tools

---
+++ Bot.Tools/DelegatedProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Bot.Tools.Interfaces;
+
+namespace Bot.Tools {
+  public class DelegatedProvider<TResult> : IProvider<TResult> {
+    private readonly Func<TResult> _get;
+
+    public DelegatedProvider(Func<TResult> get) {
+      _get = get;
+    }
+
+    public TResult Get() =>
+      _get();
+  }
+}
---
+++ Bot.Tools/Interfaces/IProvider.cs
@@ -0,0 +1,11 @@
+﻿namespace Bot.Tools.Interfaces {
+  public interface IProvider<out T> {
+    /// <summary>
+    /// Gets an instance of <typeparamref name="T"/>
+    /// </summary>
+    /// <remarks>
+    /// The implementer of this <see cref="IProvider{T}"/> is responsible for managing the lifestyle of the returned object
+    /// </remarks>
+    T Get();
+  }
+}
