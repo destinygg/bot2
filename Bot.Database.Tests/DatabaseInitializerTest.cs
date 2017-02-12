8e538661c90bc135efe62e03802cb739b9a92a84
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-02-12 00:05:30 -0600

    Added DatabaseInitializerTest and tested EnsureCreated and EnsureDeleted

---
+++ Bot.Database.Tests/DatabaseInitializerTest.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using Bot.Tests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bot.Database.Tests {
+  [TestClass]
+  public class DatabaseInitializerTest {
+    [TestMethod]
+    public void EnsureCreated_Always_CreatesFile() {
+      var cm = new ContainerManager();
+      var databaseInitializer = cm.Container.GetInstance<DatabaseInitializer>();
+      var path = "Bot.db";
+      if (File.Exists(path))
+        File.Delete(path);
+      Assert.IsFalse(File.Exists(path));
+
+      databaseInitializer.EnsureCreated();
+
+      Assert.IsTrue(File.Exists(path));
+    }
+
+    [TestMethod]
+    public void EnsureDeleted_Always_DeletesFile() {
+      var cm = new ContainerManager();
+      var databaseInitializer = cm.Container.GetInstance<DatabaseInitializer>();
+      var path = "Bot.db";
+      if (!File.Exists(path))
+        databaseInitializer.EnsureCreated();
+      Assert.IsTrue(File.Exists(path));
+
+      databaseInitializer.EnsureDeleted();
+
+      Assert.IsFalse(File.Exists(path));
+    }
+
+  }
+}
