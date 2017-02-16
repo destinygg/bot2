9314e9e58e640a1e87258ceda00409c156018acd
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-02-16 02:38:12 -0600

    Added log4net; configured a ConsoleAppender; used it to log startup/initialization

--- Bot.Main.Moderate/Program.cs
+++ Bot.Main.Moderate/Program.cs
@@ -1,11 +1,21 @@
 ﻿using System;
+using log4net;
+
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
 
 namespace Bot.Main.Moderate {
   class Program {
     static void Main(string[] args) {
+      var logger = LogManager.GetLogger("Main");
+      logger.Info("Welcome to Bot!");
+      logger.Info("Initializing...");
+
       var containerManager = new ContainerManager();
       var data = containerManager.SampleReceived;
       var pipeline = containerManager.Pipeline;
+
+      logger.Info("Initialization complete.");
+      logger.Info("Running...\r\n\r\n");
 
       pipeline.Run(data);
 
