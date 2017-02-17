46904ecded3c2c03c1fd603bc5876ea6bf4a609a
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-02-16 21:08:47 -0600

    Created LogEntry and LoggingEventType

---
+++ Bot.Tools/LogEntry.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Bot.Tools {
+  public class LogEntry {
+    public LoggingEventType Severity { get; }
+    public string Message { get; }
+    public Exception Exception { get; }
+
+    public LogEntry(LoggingEventType severity, string message, Exception exception = null) {
+      Severity = severity;
+      Message = message;
+      Exception = exception;
+    }
+
+  }
+}
---
+++ Bot.Tools/LoggingEventType.cs
@@ -0,0 +1,3 @@
+﻿namespace Bot.Tools {
+  public enum LoggingEventType { Debug, Information, Warning, Error, Fatal };
+}
