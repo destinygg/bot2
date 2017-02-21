27e4f8f93209da4d3efc0cb5fd42016fc1cb2882
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-02-20 23:24:57 -0600

    Fleshed out ModCommandRegex's !nuke this test

--- Bot.Logic.Tests/ModCommandRegexTest.cs
+++ Bot.Logic.Tests/ModCommandRegexTest.cs
@@ -1,15 +1,17 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Bot.Logic.Tests {
   [TestClass]
   public class ModCommandRegexTest {
     [TestMethod]
-    public void TestMethod1() {
-      var mc = new ModCommandRegex();
-      var nuke = mc.Nuke;
-      Assert.IsTrue(nuke.IsMatch("!nuke this"));
-      var s = nuke.Match("!nuke this").Groups[1];
+    public void ModCommandRegex_nuke_this_AlwaysMatches() {
+      var testPhrase = "!nuke this";
+      var modCommandRegex = new ModCommandRegex();
+
+      var nukeRegex = modCommandRegex.Nuke;
+
+      Assert.IsTrue(nukeRegex.IsMatch(testPhrase));
     }
+
   }
 }
--- Bot.Logic/SnapshotVisitor/BaseSnapshotVisitor.cs
+++ Bot.Logic/SnapshotVisitor/BaseSnapshotVisitor.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
 using Bot.Models;
 using Bot.Models.Interfaces;
-using Bot.Tools.Interfaces;
 using Bot.Tools.Logging;
 using Microsoft.CSharp.RuntimeBinder;
 
