aa94a3408a8398a128001625f2de0c6f25cb8660
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-02-21 06:02:06 -0600

    Fully implemented ReceivedVisitor. Everything runs!

--- Bot.Logic/ReceivedVisitor/ReceivedVisitor.cs
+++ Bot.Logic/ReceivedVisitor/ReceivedVisitor.cs
@@ -3,20 +3,17 @@ using Bot.Models.Interfaces;
 
 namespace Bot.Logic.ReceivedVisitor {
   public class ReceivedVisitor : IReceivedVisitor<DelegatedSnapshotFactory> {
-    public DelegatedSnapshotFactory Visit(IReceived<Civilian, PublicMessage> t) {
-      throw new System.NotImplementedException();
-    }
+    public DelegatedSnapshotFactory Visit(IReceived<Civilian, PublicMessage> message) =>
+      new DelegatedSnapshotFactory(context => new Snapshot<Civilian, PublicMessage>(message, context));
 
-    public DelegatedSnapshotFactory Visit(IReceived<Moderator, PublicMessage> t) {
-      throw new System.NotImplementedException();
-    }
+    public DelegatedSnapshotFactory Visit(IReceived<Moderator, PublicMessage> message) =>
+      new DelegatedSnapshotFactory(context => new Snapshot<Moderator, PublicMessage>(message, context));
 
-    public DelegatedSnapshotFactory Visit(IReceived<Moderator, ErrorMessage> t) {
-      throw new System.NotImplementedException();
-    }
+    public DelegatedSnapshotFactory Visit(IReceived<Moderator, ErrorMessage> message) =>
+      new DelegatedSnapshotFactory(context => new Snapshot<Moderator, ErrorMessage>(message, context));
 
-    public DelegatedSnapshotFactory Visit(IReceived<Moderator, Pardon> t) {
-      throw new System.NotImplementedException();
-    }
+    public DelegatedSnapshotFactory Visit(IReceived<Moderator, Pardon> pardon) =>
+      new DelegatedSnapshotFactory(context => new Snapshot<Moderator, Pardon>(pardon, context));
+
   }
 }
