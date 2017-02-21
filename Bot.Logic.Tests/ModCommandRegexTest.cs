bdb9ee8fe334fbe2a3c0a172519618970fca2589
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-02-20 23:23:29 -0600

    UnitTest1 => ModCommandRegexTest

--- Bot.Logic.Tests/ModCommandRegexTest.cs
+++ Bot.Logic.Tests/ModCommandRegexTest.cs
@@ -3,7 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Bot.Logic.Tests {
   [TestClass]
-  public class UnitTest1 {
+  public class ModCommandRegexTest {
     [TestMethod]
     public void TestMethod1() {
       var mc = new ModCommandRegex();
