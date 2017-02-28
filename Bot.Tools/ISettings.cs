2c2a3230953da7a617d6bf73b944a57ef68ce553
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-02-27 21:44:47 -0600

    Removed static from Settings and extracted an interface, ISettings

---
+++ Bot.Tools/ISettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Bot.Tools {
+  public interface ISettings {
+    int ContextSize { get; }
+    string DebuggerTimeZone { get; }
+    string DestinyTimeZone { get; }
+    bool IsMono { get; }
+    TimeSpan NukeBlastRadius { get; }
+    double NukeMinimumStringSimilarity { get; }
+    string SqlitePath { get; }
+  }
+}
--- Bot.Tools/Settings.cs
+++ Bot.Tools/Settings.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 
 namespace Bot.Tools {
-  public static class Settings {
-    public const int ContextSize = 1000;
-    public const string SqlitePath = "Bot.sqlite";
-    public static TimeSpan NukeBlastRadius => TimeSpan.FromMinutes(5);
-    public const double NukeMinimumStringSimilarity = 0.7;
-    public static bool IsMono = Type.GetType("Mono.Runtime") != null;
-    public static string DestinyTimeZone = IsMono ? "US/Central" : "Central Standard Time"; // http://mono.1490590.n4.nabble.com/Cross-platform-time-zones-td1507630.html
-    public static string DebuggerTimeZone = IsMono ? "US/Central" : "Central Standard Time";
+  public class Settings : ISettings {
+    public int ContextSize => 1000;
+    public string SqlitePath => "Bot.sqlite";
+    public TimeSpan NukeBlastRadius => TimeSpan.FromMinutes(5);
+    public double NukeMinimumStringSimilarity => 0.7;
+    public bool IsMono => Type.GetType("Mono.Runtime") != null;
+    public string DestinyTimeZone => IsMono ? "US/Central" : "Central Standard Time"; // http://mono.1490590.n4.nabble.com/Cross-platform-time-zones-td1507630.html
+    public string DebuggerTimeZone => IsMono ? "US/Central" : "Central Standard Time";
   }
 }
