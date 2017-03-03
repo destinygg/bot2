a3eb53f1c55e4a349a386aff9f1274b4fd691b24
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-03-02 23:45:29 -0600

    Created SendableError

---
+++ Bot.Models/Sendable/SendableError.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+using Bot.Models.Interfaces;
+
+namespace Bot.Models.Sendable {
+  [DebuggerDisplay("Sending: {Text}")]
+  public class SendableError : ISendable<ErrorMessage> {
+
+    public SendableError(string text) {
+      Transmission = new ErrorMessage(text);
+    }
+
+    public ErrorMessage Transmission { get; }
+    public string Text => Transmission.Text;
+    public TResult Accept<TResult>(ISendableVisitor<TResult> visitor) => visitor.Visit(this);
+  }
+}
