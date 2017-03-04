b4b54e2b8e0dc2755c05635c940d31638ea96fab
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-03-04 14:06:42 -0600

    Used the new ContextBuilder in Nuke tests; also added some Aegis tests

--- Bot.Logic.Tests/ModCommandLogicTests_Nuke.cs
+++ Bot.Logic.Tests/ModCommandLogicTests_Nuke.cs
@@ -3,48 +3,77 @@ using System.Linq;
 using Bot.Logic.Interfaces;
 using Bot.Logic.Tests.Helper;
 using Bot.Models.Sendable;
+using Bot.Tools;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Bot.Logic.Tests {
   [TestClass]
   public class ModCommandLogicTests_Nuke {
 
+    private readonly ContextBuilder _messagesWithDifferentCasing = (ContextBuilder) new ContextBuilder()
+      .RadiusIs("1:00:00")
+      .SubsequentlySpacedBy(TimeSpan.FromMinutes(1))
+      .PublicMessage("1")
+      .TargetedMessage("MESSAGE")
+      .TargetedMessage("message")
+      .PublicMessage("4");
+
     [TestMethod]
-    public void SimpleNuke() {
-      var contextBuilder = new ContextBuilder();
-      var context = contextBuilder
-        .SubsequentlySpacedBy(TimeSpan.FromTicks(1))
-        .PublicMessage("1")
-        .TargetedMessage("MESSAGE")
-        .TargetedMessage("message")
-        .PublicMessage("4").Build();
-      var nukeBlastRadius = TimeSpan.FromMinutes(100);
-      var container = NukeHelper.GetContainer(contextBuilder.NextTimestamp(), nukeBlastRadius);
+    public void Nuke_MessagesWithDifferentCasing_IsCaseInsensitive() {
+      var container = NukeHelper.GetContainer(_messagesWithDifferentCasing.NextTimestamp(), _messagesWithDifferentCasing.NukeBlastRadius);
       var logic = container.GetInstance<ModCommandLogic>();
       var factory = container.GetInstance<IReceivedFactory>();
 
-      var nukeResults = logic.Nuke(context, factory.ParsedNuke("!nuke10m message"));
+      var nukeResults = logic.Nuke(_messagesWithDifferentCasing.Build(), factory.ParsedNuke("!nuke10m message"));
 
       var nukedUsers = nukeResults.OfType<SendableMute>().Select(umb => umb.Target);
-      contextBuilder.VerifyTargeted(nukedUsers);
+      _messagesWithDifferentCasing.VerifyTargeted(nukedUsers);
     }
 
     [TestMethod]
-    public void OutOfRangeNuke() {
-      var contextBuilder = new ContextBuilder();
-      var context = contextBuilder
-        .InsertAt(" 0:59:59.9999999").PublicMessage("message") //Out of nuke range
-        .InsertAt(" 1:00:00.0000000").TargetedMessage("message").Build();//Inside nuke range
-      var time = "  1:05:00.0000000";
-      var radius = "0:05";
-      var container = NukeHelper.GetContainer(time, radius);
+    public void Aegis_MessagesWithDifferentCasing_IsCaseInsensitive() {
+      var container = NukeHelper.GetContainer(_messagesWithDifferentCasing.NextTimestamp(), _messagesWithDifferentCasing.NukeBlastRadius);
+      var logic = container.GetInstance<ModCommandLogic>();
+      var factory = container.GetInstance<IReceivedFactory>();
+      var nuke = factory.ModPublicReceivedMessage("!nuke10m message");
+      var contextWithNuke = _messagesWithDifferentCasing.Build().Concat(nuke.Wrap()).ToList();
+
+      var aegisResults = logic.Aegis(contextWithNuke);
+
+      var aegisedUsers = aegisResults.OfType<SendablePardon>().Select(umb => umb.Target);
+      _messagesWithDifferentCasing.VerifyTargeted(aegisedUsers);
+    }
+
+    private readonly ContextBuilder _messagesInAndOutOfRadius = (ContextBuilder) new ContextBuilder()
+        .InsertAt("0:59:59.9999999").PublicMessage("message") //Out of nuke range
+        .InsertAt("1:00:00.0000000").TargetedMessage("message")
+        .BuildAt(" 1:05:00.0000000")
+        .RadiusIs("0:05");
+
+    [TestMethod]
+    public void Nuke_MessagesInAndOutOfRadius_TargetsOnlyInRadius() {
+      var container = NukeHelper.GetContainer(_messagesInAndOutOfRadius.BuiltAt, _messagesInAndOutOfRadius.NukeBlastRadius);
       var logic = container.GetInstance<ModCommandLogic>();
       var factory = container.GetInstance<IReceivedFactory>();
 
-      var nukeResults = logic.Nuke(context, factory.ParsedNuke("!nuke10m message"));
+      var nukeResults = logic.Nuke(_messagesInAndOutOfRadius.Build(), factory.ParsedNuke("!nuke10m message"));
 
       var nukedUsers = nukeResults.OfType<SendableMute>().Select(umb => umb.Target);
-      contextBuilder.VerifyTargeted(nukedUsers);
+      _messagesInAndOutOfRadius.VerifyTargeted(nukedUsers);
+    }
+
+    [TestMethod]
+    public void Aegis_MessagesInAndOutOfRadius_TargetsOnlyInRadius() {
+      var container = NukeHelper.GetContainer(_messagesInAndOutOfRadius.BuiltAt, _messagesInAndOutOfRadius.NukeBlastRadius);
+      var logic = container.GetInstance<ModCommandLogic>();
+      var factory = container.GetInstance<IReceivedFactory>();
+      var nuke = factory.ModPublicReceivedMessage("!nuke10m message");
+      var contextWithNuke = _messagesInAndOutOfRadius.Build().Concat(nuke.Wrap()).ToList();
+
+      var aegisResults = logic.Aegis(contextWithNuke);
+
+      var aegisedUsers = aegisResults.OfType<SendablePardon>().Select(umb => umb.Target);
+      _messagesInAndOutOfRadius.VerifyTargeted(aegisedUsers);
     }
 
   }
