1ce579d806f8790777a12385be6315155ce8d7e6
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-03-09 21:21:03 -0600

    Added RepositoryHelper

---
+++ Bot.Repository.Tests/RepositoryHelper.cs
@@ -0,0 +1,26 @@
+﻿using Bot.Tests;
+using Bot.Tools;
+using NSubstitute;
+using SimpleInjector;
+
+namespace Bot.Repository.Tests {
+  internal static class RepositoryHelper {
+
+    public static Container GetContainer(string sqliteName) {
+
+      var settings = Substitute.For<ISettings>();
+      settings.SqlitePath.Returns($"{sqliteName}.sqlite");
+
+      var containerManager = new TestContainerManager(
+        container => {
+          var settingsServiceRegistration = Lifestyle.Singleton.CreateRegistration(() => settings, container);
+          container.RegisterConditional(typeof(ISettings), settingsServiceRegistration, pc => !pc.Handled);
+        });
+
+      containerManager.Container.GetInstance<RepositoryInitializer>().RecreateWithMasterData();
+
+      return containerManager.Container;
+    }
+
+  }
+}
