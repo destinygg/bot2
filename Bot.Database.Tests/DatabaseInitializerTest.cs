c0683dbd81d16341545d72674aff6546de8f994d
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-03-10 01:50:00 -0600

    Improved the robustness of DatabaseInitializerTest

--- Bot.Database.Tests/DatabaseInitializerTest.cs
+++ Bot.Database.Tests/DatabaseInitializerTest.cs
@@ -1,15 +1,26 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using Bot.Tests;
+using Bot.Tools;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using SimpleInjector;
 
 namespace Bot.Database.Tests {
   [TestClass]
   public class DatabaseInitializerTest {
     [TestMethod]
     public void EnsureCreated_Always_CreatesFile() {
-      var cm = new TestContainerManager();
-      var databaseInitializer = cm.Container.GetInstance<DatabaseInitializer>();
-      var path = "Bot.db";
+      var path = Path.Combine(Directory.GetCurrentDirectory(), Guid.NewGuid() + ".sqlite");
+      var settings = Substitute.For<ISettings>();
+      settings.SqlitePath.Returns(path);
+      Console.WriteLine(path);
+      var containerManager = new TestContainerManager(
+        container => {
+          var settingsServiceRegistration = Lifestyle.Singleton.CreateRegistration(() => settings, container);
+          container.RegisterConditional(typeof(ISettings), settingsServiceRegistration, pc => !pc.Handled);
+        });
+      var databaseInitializer = containerManager.Container.GetInstance<DatabaseInitializer>();
       if (File.Exists(path))
         File.Delete(path);
       Assert.IsFalse(File.Exists(path));
@@ -21,9 +32,16 @@ namespace Bot.Database.Tests {
 
     [TestMethod]
     public void EnsureDeleted_Always_DeletesFile() {
-      var cm = new TestContainerManager();
-      var databaseInitializer = cm.Container.GetInstance<DatabaseInitializer>();
-      var path = "Bot.db";
+      var path = Path.Combine(Directory.GetCurrentDirectory(), Guid.NewGuid() + ".sqlite");
+      var settings = Substitute.For<ISettings>();
+      settings.SqlitePath.Returns(path);
+      Console.WriteLine(path);
+      var containerManager = new TestContainerManager(
+        container => {
+          var settingsServiceRegistration = Lifestyle.Singleton.CreateRegistration(() => settings, container);
+          container.RegisterConditional(typeof(ISettings), settingsServiceRegistration, pc => !pc.Handled);
+        });
+      var databaseInitializer = containerManager.Container.GetInstance<DatabaseInitializer>();
       if (!File.Exists(path))
         databaseInitializer.EnsureCreated();
       Assert.IsTrue(File.Exists(path));
