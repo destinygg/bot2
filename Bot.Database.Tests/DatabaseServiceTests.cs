c3c0d56861d78835a417916bccf47a8d24133ee1
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-03-12 12:59:44 -0500

    Used DatabaseHelper in DatabaseServiceTests, dropping the TestInitialize

--- Bot.Database.Tests/DatabaseServiceTests.cs
+++ Bot.Database.Tests/DatabaseServiceTests.cs
@@ -7,25 +7,18 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Bot.Database.Tests {
   [TestClass]
   public class DatabaseServiceTests {
-    private IDatabaseService<IBotDbContext> _databaseService;
-
-    [TestInitialize]
-    public void Initialize() {
-      var containerManager = new TestContainerManager();
-      var databaseInitializer = containerManager.Container.GetInstance<DatabaseInitializer>();
-      databaseInitializer.Recreate();
-      _databaseService = containerManager.Container.GetInstance<IDatabaseService<IBotDbContext>>();
-    }
 
     [TestMethod]
     public void DatabaseServiceAddingIncompleteEntity_Always_ThrowsForeignKeyException() {
+      var databaseService = DatabaseHelper.GetContainerWithInitializedAndIsolatedDatabase().GetInstance<IDatabaseService<IBotDbContext>>();
+
       var punishedUser = new PunishedUser {
         Count = 1,
         AutoPunishmentId = 1,
         UserId = 1,
       };
 
-      var exception = TestHelper.AssertCatch<DbUpdateException>(() => _databaseService.Command(db => db.PunishedUsers.Add(punishedUser)));
+      var exception = TestHelper.AssertCatch<DbUpdateException>(() => databaseService.Command(db => db.PunishedUsers.Add(punishedUser)));
 
       Assert.AreEqual("SQLite Error 19: 'FOREIGN KEY constraint failed'.", exception.InnerException.Message);
     }
