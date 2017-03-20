c4d4df0f4213e277bc55beaaedeeb3a488ad0d98
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-03-19 23:31:36 -0500

    Created AutoPunishment, PunishedUser, and StateInteger

---
+++ Bot.Models/PunishedUser.cs
@@ -0,0 +1,9 @@
+﻿namespace Bot.Models {
+  public class PunishedUser {
+    public int Id { get; set; }
+    public int AutoPunishmentId { get; set; }
+    public virtual AutoPunishment AutoPunishment { get; set; }
+    public string Nick { get; set; }
+    public int Count { get; set; }
+  }
+}
