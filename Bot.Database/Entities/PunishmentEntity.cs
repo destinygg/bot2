186e1594f3ecf5fab9c5977fc838a46d5346edc2
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-03-19 23:25:54 -0500

    Appended Entity to filenames

--- Bot.Database/Entities/PunishmentEntity.cs
+++ Bot.Database/Entities/PunishmentEntity.cs
@@ -3,10 +3,14 @@
 namespace Bot.Database.Entities {
 
   public class AutoPunishmentEntity {
+    public AutoPunishmentEntity() {
+      PunishedUsers = new HashSet<PunishedUserEntity>();
+    }
     public int Id { get; set; }
     public string Term { get; set; }
     public AutoPunishmentType Type { get; set; }
     public long Duration { get; set; }
+
     public virtual ICollection<PunishedUserEntity> PunishedUsers { get; set; }
   }
 
