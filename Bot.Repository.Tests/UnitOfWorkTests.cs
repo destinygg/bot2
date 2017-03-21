d7190f39a07c23e7ecc664d57e1cec0d93f8dd78
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-03-21 17:54:25 -0500

    Deleted PunishedUserRepository

--- Bot.Repository.Tests/UnitOfWorkTests.cs
+++ Bot.Repository.Tests/UnitOfWorkTests.cs
@@ -9,26 +9,26 @@ namespace Bot.Repository.Tests {
   [TestClass]
   public class UnitOfWorkTests {
 
-    [TestMethod]
-    public void UpdatePunishedUser() {
-      var unitOfWorkService = RepositoryHelper.GetContainerWithInitializedAndIsolatedRepository().GetInstance<IQueryCommandService<IUnitOfWork>>();
-      var nick = TestHelper.RandomString();
-      var oldCount = TestHelper.RandomInt();
-      var punishedUserWrite = new PunishedUser {
-        Nick = nick,
-        AutoPunishment = new AutoPunishment(),
-        Count = oldCount,
-      };
-      unitOfWorkService.Command(u => u.PunishedUsers.Add(punishedUserWrite));
-      var userToUpdate = unitOfWorkService.Query(u => u.PunishedUsers.SingleOrDefault(x => x.Nick == nick));
-      var newCount = oldCount + 1;
-      userToUpdate.Count = newCount;
+    //[TestMethod]
+    //public void UpdatePunishedUser() {
+    //  var unitOfWorkService = RepositoryHelper.GetContainerWithInitializedAndIsolatedRepository().GetInstance<IQueryCommandService<IUnitOfWork>>();
+    //  var nick = TestHelper.RandomString();
+    //  var oldCount = TestHelper.RandomInt();
+    //  var punishedUserWrite = new PunishedUser {
+    //    Nick = nick,
+    //    AutoPunishment = new AutoPunishment(),
+    //    Count = oldCount,
+    //  };
+    //  unitOfWorkService.Command(u => u.PunishedUsers.Add(punishedUserWrite));
+    //  var userToUpdate = unitOfWorkService.Query(u => u.PunishedUsers.SingleOrDefault(x => x.Nick == nick));
+    //  var newCount = oldCount + 1;
+    //  userToUpdate.Count = newCount;
 
-      unitOfWorkService.Command(u => u.PunishedUsers.Update(userToUpdate));
+    //  unitOfWorkService.Command(u => u.PunishedUsers.Update(userToUpdate));
 
-      var updatedUser = unitOfWorkService.Query(u => u.PunishedUsers.SingleOrDefault(x => x.Nick == nick));
-      Assert.AreEqual(updatedUser.Count, newCount);
-    }
+    //  var updatedUser = unitOfWorkService.Query(u => u.PunishedUsers.SingleOrDefault(x => x.Nick == nick));
+    //  Assert.AreEqual(updatedUser.Count, newCount);
+    //}
 
   }
 }
