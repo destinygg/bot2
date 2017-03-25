775f166dc7e833e804497ca0ecf8225c07d839e7
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-03-25 02:43:33 -0500

    Updated AutoPunishment and PunishedUser to make them immutable with ToEntity() and ctors taking entities

--- Bot.Models/AutoPunishment.cs
+++ Bot.Models/AutoPunishment.cs
@@ -1,12 +1,31 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Bot.Database.Entities;
 
 namespace Bot.Models {
   public class AutoPunishment {
-    public int Id { get; set; }
-    public string Term { get; set; }
-    public AutoPunishmentType Type { get; set; }
-    public long Duration { get; set; }
-    public virtual ICollection<PunishedUser> PunishedUsers { get; set; }
+
+    public AutoPunishment(AutoPunishmentEntity entity) {
+      Id = entity.Id;
+      Term = entity.Term;
+      Type = entity.Type;
+      Duration = entity.Duration;
+      PunishedUsers = entity.PunishedUsers.Select(x => new PunishedUser(x, this)).ToList();
+    }
+
+    public int Id { get; }
+    public string Term { get; }
+    public AutoPunishmentType Type { get; }
+    public long Duration { get; }
+    public ICollection<PunishedUser> PunishedUsers { get; }
+
+    public AutoPunishmentEntity ToEntity() => new AutoPunishmentEntity {
+      Id = Id,
+      Term = Term,
+      Type = Type,
+      Duration = Duration,
+      PunishedUsers = PunishedUsers.Select(u => u.ToEntity()).ToList(),
+    };
+
   }
 }
--- Bot.Models/PunishedUser.cs
+++ Bot.Models/PunishedUser.cs
@@ -1,9 +1,26 @@
-﻿namespace Bot.Models {
+﻿using Bot.Database.Entities;
+
+namespace Bot.Models {
   public class PunishedUser {
-    public int Id { get; set; }
-    public int AutoPunishmentId { get; set; }
-    public virtual AutoPunishment AutoPunishment { get; set; }
-    public string Nick { get; set; }
-    public int Count { get; set; }
+
+    public PunishedUser(PunishedUserEntity entity, AutoPunishment autoPunishment) {
+      Id = entity.Id;
+      Nick = entity.Nick;
+      Count = entity.Count;
+      AutoPunishment = autoPunishment;
+      AutoPunishment = autoPunishment;
+    }
+
+    public int Id { get; }
+    public string Nick { get; }
+    public int Count { get; }
+    public AutoPunishment AutoPunishment { get; }
+
+    public PunishedUserEntity ToEntity() => new PunishedUserEntity {
+      Id = Id,
+      Nick = Nick,
+      Count = Count,
+    };
+
   }
 }
