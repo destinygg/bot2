6ad0bee74b6236c794882fbb8d0c019735973944
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-03-31 23:06:55 -0500

    Added Merge and a small test for it

---
+++ Bot.Tools.Tests/MergeTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bot.Tools.Tests {
+  [TestClass]
+  public class MergeTests {
+    [TestMethod]
+    public void SimpleMerge() {
+      var a = new List<Person> { new Person(1, "a"), new Person(2, "c") };
+      var b = new List<Person> { new Person(1, "b"), new Person(9, "b") };
+
+      b.Merge(
+          source: a,
+          predicate: (person1, person2) => person1.Id == person2.Id,
+          create: person => person,
+          delete: person => b.Remove(person),
+          add: person => b.Add(person),
+          update: (d, s) => {
+            d.Name = s.Name;
+          });
+
+      Assert.IsTrue(a.Select(x => x.Id).SequenceEqual(b.Select(x => x.Id)));
+      Assert.IsTrue(a.Select(x => x.Name).SequenceEqual(b.Select(x => x.Name)));
+    }
+
+    private class Person {
+      public Person(int id, string name) {
+        Id = id;
+        Name = name;
+      }
+
+      public int Id { get; }
+      public string Name { get; set; }
+      public override string ToString() => Id + "." + Name;
+    }
+
+  }
+}
---
+++ Bot.Tools/MergeExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bot.Tools {
+  public static class MergeExtensions {
+
+    public static void Merge<TDest, TSource>(this IEnumerable<TDest> destination,
+        IEnumerable<TSource> source,
+        Func<TDest, TSource, bool> predicate,
+        Func<TSource, TDest> create,
+        Action<TDest> delete,
+        Action<TDest> add,
+        Action<TDest, TSource> update
+    ) {
+      //List < (TDest d, TSource s)> updates = null; // todo Update to VS2017 and use the new tuple syntax
+      List<Tuple<TDest, TSource>> updates = null;
+      List<TSource> adds = null;
+      List<TDest> deletes = null;
+
+      source = source ?? Enumerable.Empty<TSource>();
+      if (update != null)
+        updates = (
+            from d in destination
+            from s in source
+            where predicate(d, s)
+            //select (d, s)
+            select Tuple.Create(d, s)
+        ).ToList();
+      if (add != null && create != null)
+        adds = source.Where(s => !destination.Any(d => predicate(d, s))).ToList();
+      if (delete != null) deletes = destination.Where(d => !source.Any(s => predicate(d, s))).ToList();
+
+      //updates?.ForEach(t => update(t.d, t.s));
+      updates?.ForEach(t => update(t.Item1, t.Item2));
+      deletes?.ForEach(delete);
+      adds?.ForEach(s => {
+        TDest d = create(s);
+        update?.Invoke(d, s);
+        add.Invoke(d);
+      });
+    }
+
+  }
+}
