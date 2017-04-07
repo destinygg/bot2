8d690607fbac7e63c0c2367528f16b7ab10ef30f
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-04-06 21:49:52 -0500

    Created IPunishedUserRepositry, added it to IUnitOfWork, and tested its Increment

---
+++ Bot.Repository/PunishedUserRepository.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using Bot.Database.Entities;
+using Bot.Models;
+using Bot.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bot.Repository {
+  public class PunishedUserRepository : IPunishedUserRepository {
+    private readonly DbSet<PunishedUserEntity> _punishedUserEntities;
+    private readonly DbSet<AutoPunishmentEntity> _autoPunishmentEntities;
+
+    public PunishedUserRepository(DbSet<PunishedUserEntity> punishedUserEntities, DbSet<AutoPunishmentEntity> autoPunishmentEntities) {
+      _punishedUserEntities = punishedUserEntities;
+      _autoPunishmentEntities = autoPunishmentEntities;
+    }
+
+    public PunishedUser GetUser(string nick) =>
+        new PunishedUser(_punishedUserEntities.Include(x => x.AutoPunishmentEntity).Single(f => f.Nick == nick));
+
+    public void Increment(string nick, string term) {
+      var punishedUserEntity = _punishedUserEntities.Include(x => x.AutoPunishmentEntity).SingleOrDefault(pue => pue.Nick == nick);
+      if (punishedUserEntity == null) {
+        var autoPunishmentEntity = _autoPunishmentEntities.Single(ap => ap.Term == term);
+        _punishedUserEntities.Add(new PunishedUserEntity {
+          Nick = nick,
+          Count = 1,
+          AutoPunishmentId = autoPunishmentEntity.Id,
+        });
+      } else {
+        punishedUserEntity.Count++;
+        _punishedUserEntities.Update(punishedUserEntity);
+      }
+
+    }
+
+  }
+}
