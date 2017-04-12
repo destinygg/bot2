d84b358cbe6190cd36b71c62e66964c6e9799ef1
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-04-11 22:24:11 -0500

    Added TestableSender

---
+++ Bot.Pipeline.Tests/TestableSender.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Bot.Models.Interfaces;
+using Bot.Pipeline.Interfaces;
+
+namespace Bot.Pipeline.Tests {
+  public class TestableSender : ICommandHandler<IEnumerable<ISendable<ITransmittable>>> {
+
+    public void Handle(IEnumerable<ISendable<ITransmittable>> sendables) {
+      foreach (var sendable in sendables) {
+        Console.WriteLine(sendable);
+        Outbox.Add(sendable);
+      }
+    }
+
+    public IList<ISendable<ITransmittable>> Outbox { get; } = new List<ISendable<ITransmittable>>();
+  }
+}
