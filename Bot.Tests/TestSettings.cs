b487b1f359993664f3691ac8240182c6e5d094c5
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-04-17 18:17:21 -0500

    Created TestSettings

---
+++ Bot.Tests/TestSettings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Bot.Tools;
+
+namespace Bot.Tests {
+  public class TestSettings : ISettings {
+
+    private static readonly Settings Settings = new Settings();
+
+    public int ContextSize { get; set; } = Settings.ContextSize;
+    public string SqlitePath { get; set; } = Settings.SqlitePath;
+    public TimeSpan NukeBlastRadius { get; set; } = Settings.NukeBlastRadius;
+    public TimeSpan NukeMaximumLinger { get; set; } = Settings.NukeMaximumLinger;
+    public double NukeMinimumStringSimilarity { get; set; } = Settings.NukeMinimumStringSimilarity;
+    public bool IsMono { get; set; } = Settings.IsMono;
+    public string DestinyTimeZone { get; set; } = Settings.DestinyTimeZone;
+    public string DebuggerTimeZone { get; set; } = Settings.DebuggerTimeZone;
+    public double MinimumPunishmentSimilarity { get; set; } = Settings.MinimumPunishmentSimilarity;
+    public TimeSpan CivilianCommandInterval { get; set; } = Settings.CivilianCommandInterval;
+  }
+}
