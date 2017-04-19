649a3950dfcb4172795741fcc2b936c28aaa3022
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-04-19 11:10:02 -0500

    Added a test for Log4NetLogger that prints an exception to the output for easy format updating. Added log4net.config as a reference-by-link

---
+++ Bot.Pipeline.Tests/Log4NetLoggerTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using Bot.Tests;
+using log4net.Config;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bot.Pipeline.Tests {
+  [TestClass]
+  public class Log4NetLoggerTests {
+
+    [TestMethod]
+    public void Log4NetLogger_Displays_Exceptions() {
+      XmlConfigurator.Configure(new FileInfo(@"log4net.config"));
+      var containerManager = new TestContainerManager();
+      var log4NetLogger = containerManager.Container.GetInstance<Log4NetLogger<Log4NetLoggerTests>>();
+      try {
+        var zero = 0;
+        var undefined = 1 / zero;
+      } catch (Exception e) {
+        log4NetLogger.LogError("An error message", e);
+      }
+    }
+
+  }
+}
