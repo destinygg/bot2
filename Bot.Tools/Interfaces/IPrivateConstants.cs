f5e8022bf9b32abe1ded561e6d113cbe87fb3032
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-04-22 00:34:06 -0500

    Added IPrivateConstants

---
+++ Bot.Tools/Interfaces/IPrivateConstants.cs
@@ -0,0 +1,21 @@
+﻿namespace Bot.Tools.Interfaces {
+  public interface IPrivateConstants {
+    string TestAccountWebsocketAuth { get; }
+    string BotWebsocketAuth { get; }
+    string ImgurAuthHeader { get; }
+    string TwitterAccessToken { get; }
+    string TwitterAccessTokenSecret { get; }
+    string TwitterConsumerKey { get; }
+    string TwitterConsumerSecret { get; }
+    string LastFmApiKey { get; }
+    string Youtube { get; }
+    string TwitchNick { get; }
+    string TwitchOauth { get; }
+    string IrcFloodBypassPassword { get; }
+    string BattleNet { get; }
+    string GoogleClientId { get; }
+    string GoogleClientSecret { get; }
+    string GoogleKey { get; }
+    string TwitchClientId { get; }
+  }
+}
