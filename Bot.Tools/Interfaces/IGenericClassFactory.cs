412fa86015841d013aaef97dc8b5faa41d01b85f
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-04-22 22:02:05 -0500

    Added IGenericClassFactory

---
+++ Bot.Tools/Interfaces/IGenericClassFactory.cs
@@ -0,0 +1,17 @@
+﻿namespace Bot.Tools.Interfaces {
+
+  public interface IGenericClassFactory<in T> {
+    TResult Create<TResult>(T input)
+      where TResult : class;
+  }
+
+  public interface IGenericClassFactory<in T1, in T2> {
+    TResult Create<TResult>(T1 input1, T2 input2)
+      where TResult : class;
+  }
+
+  public interface IGenericClassFactory<in T1, in T2, in T3> {
+    TResult Create<TResult>(T1 input1, T2 input2, T3 input3)
+      where TResult : class;
+  }
+}
