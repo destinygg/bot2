c9ee3dfa38a6bc7454b21ae0f5e2f0582610540f
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-04-29 00:53:02 -0500

    Created StreamStatusService and tested it

---
+++ Bot.Logic.Tests/StreamStatusServiceTests.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using Bot.Models.Json;
+using Bot.Repository.Interfaces;
+using Bot.Tests;
+using Bot.Tools.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using SimpleInjector;
+
+namespace Bot.Logic.Tests {
+  [TestClass]
+  public class StreamStatusServiceTests {
+
+    private readonly TwitchStreamStatus.RootObject _off = new TwitchStreamStatus.RootObject();
+    private readonly TwitchStreamStatus.RootObject _on = new TwitchStreamStatus.RootObject {
+      stream = new TwitchStreamStatus.Stream()
+    };
+
+    [TestMethod]
+    public void StreamStatusService_StreamNotNull_IsOn() {
+      var urlJsonParser = Substitute.For<IGenericClassFactory<string, string, string>>();
+      urlJsonParser.Create<TwitchStreamStatus.RootObject>(Arg.Any<string>(), Arg.Any<string>(), Arg.Any<string>()).Returns(_on);
+      var container = new TestContainerManager(c => {
+        var urlJsonParserRegistration = Lifestyle.Singleton.CreateRegistration(() => urlJsonParser, c);
+        c.RegisterConditional(typeof(IGenericClassFactory<string, string, string>), urlJsonParserRegistration, _ => true);
+      }).InitializeAndIsolateRepository();
+      var streamStatusService = container.GetInstance<StreamStatusService>();
+
+      var status = streamStatusService.GetStatus();
+
+      Assert.AreEqual(StreamStatus.On, status);
+    }
+
+    [TestMethod]
+    public void StreamStatusService_StreamNull_IsOffline() {
+      var urlJsonParser = Substitute.For<IGenericClassFactory<string, string, string>>();
+      urlJsonParser.Create<TwitchStreamStatus.RootObject>(Arg.Any<string>(), Arg.Any<string>(), Arg.Any<string>()).Returns(_off);
+      var container = new TestContainerManager(c => {
+        var urlJsonParserRegistration = Lifestyle.Singleton.CreateRegistration(() => urlJsonParser, c);
+        c.RegisterConditional(typeof(IGenericClassFactory<string, string, string>), urlJsonParserRegistration, _ => true);
+      }).InitializeAndIsolateRepository();
+      var streamStatusService = container.GetInstance<StreamStatusService>();
+
+      var status = streamStatusService.GetStatus();
+
+      Assert.AreEqual(StreamStatus.Off, status);
+    }
+
+    private readonly DateTime _time0 = DateTime.Today;
+    private readonly DateTime _hour1 = DateTime.Today + TimeSpan.FromHours(1);
+    private readonly DateTime _hour2 = DateTime.Today + TimeSpan.FromHours(2);
+    private readonly DateTime _hour3 = DateTime.Today + TimeSpan.FromHours(3);
+    private readonly DateTime _minu1 = DateTime.Today + TimeSpan.FromMinutes(1);
+    private readonly DateTime _minu2 = DateTime.Today + TimeSpan.FromMinutes(2);
+    private readonly DateTime _minu3 = DateTime.Today + TimeSpan.FromMinutes(3);
+    private readonly DateTime _minu4 = DateTime.Today + TimeSpan.FromMinutes(4);
+    private readonly DateTime _minu5 = DateTime.Today + TimeSpan.FromMinutes(5);
+
+    private Container _getContainer(ITimeService timeService, IGenericClassFactory<string, string, string> urlJsonParser) => new TestContainerManager(c => {
+      var timeServiceRegistration = Lifestyle.Singleton.CreateRegistration(() => timeService, c);
+      c.RegisterConditional(typeof(ITimeService), timeServiceRegistration, pc => !pc.Handled);
+      var urlJsonParserRegistration = Lifestyle.Singleton.CreateRegistration(() => urlJsonParser, c);
+      c.RegisterConditional(typeof(IGenericClassFactory<string, string, string>), urlJsonParserRegistration, _ => true);
+    }, s => s.OnOffTimeTolerance = TimeSpan.FromMinutes(2))
+      .InitializeAndIsolateRepository();
+
+    [TestMethod]
+    public void StreamStatusService_OffOnHourLater_UpdatesDatabase() {
+      var timeService = Substitute.For<ITimeService>();
+      timeService.UtcNow.Returns(_time0, _hour1);
+      var urlJsonParser = Substitute.For<IGenericClassFactory<string, string, string>>();
+      urlJsonParser.Create<TwitchStreamStatus.RootObject>(Arg.Any<string>(), Arg.Any<string>(), Arg.Any<string>()).Returns(_off, _on);
+      var container = _getContainer(timeService, urlJsonParser);
+      var streamStatusService = container.GetInstance<StreamStatusService>();
+
+      Assert.AreEqual(StreamStatus.Off, streamStatusService.GetStatus());
+      Assert.AreEqual(StreamStatus.On, streamStatusService.GetStatus());
+
+      var unitOfWork = container.GetInstance<IQueryCommandService<IUnitOfWork>>();
+      var onTime = unitOfWork.Query(u => u.StateIntegers.LatestStreamOnTime);
+      Assert.AreEqual(_hour1, onTime);
+      var status = unitOfWork.Query(u => u.StateIntegers.StreamStatus);
+      Assert.AreEqual(StreamStatus.On, status);
+    }
+
+    [TestMethod]
+    public void StreamStatusService_OffOnOffHourLater_UpdatesDatabase() {
+      var timeService = Substitute.For<ITimeService>();
+      timeService.UtcNow.Returns(_time0, _hour1, _hour2);
+      var urlJsonParser = Substitute.For<IGenericClassFactory<string, string, string>>();
+      urlJsonParser.Create<TwitchStreamStatus.RootObject>(Arg.Any<string>(), Arg.Any<string>(), Arg.Any<string>()).Returns(_off, _on, _off);
+      var container = _getContainer(timeService, urlJsonParser);
+      var streamStatusService = container.GetInstance<StreamStatusService>();
+
+      Assert.AreEqual(StreamStatus.Off, streamStatusService.GetStatus());
+      Assert.AreEqual(StreamStatus.On, streamStatusService.GetStatus());
+      Assert.AreEqual(StreamStatus.PossiblyOff, streamStatusService.GetStatus());
+
+      var unitOfWork = container.GetInstance<IQueryCommandService<IUnitOfWork>>();
+      var onTime = unitOfWork.Query(u => u.StateIntegers.LatestStreamOnTime);
+      Assert.AreEqual(_hour1, onTime);
+      var offTime = unitOfWork.Query(u => u.StateIntegers.LatestStreamOffTime);
+      Assert.AreEqual(_hour2, offTime);
+      var currentState = unitOfWork.Query(u => u.StateIntegers.StreamStatus);
+      Assert.AreEqual(StreamStatus.PossiblyOff, currentState);
+    }
+
+    [TestMethod]
+    public void StreamStatusService_OffOnOffOffHourLater_UpdatesDatabase() {
+      var timeService = Substitute.For<ITimeService>();
+      timeService.UtcNow.Returns(_time0, _hour1, _hour2, _hour3);
+      var urlJsonParser = Substitute.For<IGenericClassFactory<string, string, string>>();
+      urlJsonParser.Create<TwitchStreamStatus.RootObject>(Arg.Any<string>(), Arg.Any<string>(), Arg.Any<string>()).Returns(_off, _on, _off, _off);
+      var container = _getContainer(timeService, urlJsonParser);
+      var streamStatusService = container.GetInstance<StreamStatusService>();
+
+      Assert.AreEqual(StreamStatus.Off, streamStatusService.GetStatus());
+      Assert.AreEqual(StreamStatus.On, streamStatusService.GetStatus());
+      Assert.AreEqual(StreamStatus.PossiblyOff, streamStatusService.GetStatus());
+      Assert.AreEqual(StreamStatus.Off, streamStatusService.GetStatus());
+
+      var unitOfWork = container.GetInstance<IQueryCommandService<IUnitOfWork>>();
+      var onTime = unitOfWork.Query(u => u.StateIntegers.LatestStreamOnTime);
+      Assert.AreEqual(_hour1, onTime);
+      var possibleStreamOffTime = unitOfWork.Query(u => u.StateIntegers.LatestStreamOffTime);
+      Assert.AreEqual(_hour2, possibleStreamOffTime);
+      var currentState = unitOfWork.Query(u => u.StateIntegers.StreamStatus);
+      Assert.AreEqual(StreamStatus.Off, currentState);
+    }
+
+    [TestMethod]
+    public void StreamStatusService_OffOnOffOffOffMinuteLater_UpdatesDatabase() {
+      var timeService = Substitute.For<ITimeService>();
+      timeService.UtcNow.Returns(_time0, _minu1, _minu2, _minu3, _minu4);
+      var urlJsonParser = Substitute.For<IGenericClassFactory<string, string, string>>();
+      urlJsonParser.Create<TwitchStreamStatus.RootObject>(Arg.Any<string>(), Arg.Any<string>(), Arg.Any<string>()).Returns(_off, _on, _off, _off, _off);
+      var container = _getContainer(timeService, urlJsonParser);
+      var streamStatusService = container.GetInstance<StreamStatusService>();
+
+      Assert.AreEqual(StreamStatus.Off, streamStatusService.GetStatus());
+      Assert.AreEqual(StreamStatus.On, streamStatusService.GetStatus());
+      Assert.AreEqual(StreamStatus.PossiblyOff, streamStatusService.GetStatus());
+      Assert.AreEqual(StreamStatus.PossiblyOff, streamStatusService.GetStatus());
+      Assert.AreEqual(StreamStatus.Off, streamStatusService.GetStatus());
+
+      var unitOfWork = container.GetInstance<IQueryCommandService<IUnitOfWork>>();
+      var onTime = unitOfWork.Query(u => u.StateIntegers.LatestStreamOnTime);
+      Assert.AreEqual(_minu1, onTime);
+      var offTime = unitOfWork.Query(u => u.StateIntegers.LatestStreamOffTime);
+      Assert.AreEqual(_minu2, offTime);
+      var streamStatus = unitOfWork.Query(u => u.StateIntegers.StreamStatus);
+      Assert.AreEqual(StreamStatus.Off, streamStatus);
+    }
+
+    [TestMethod]
+    public void StreamStatusService_OffOnOffOffOffOnMinuteLater_UpdatesDatabase() {
+      var timeService = Substitute.For<ITimeService>();
+      timeService.UtcNow.Returns(_time0, _minu1, _minu2, _minu3, _minu4, _minu5);
+      var urlJsonParser = Substitute.For<IGenericClassFactory<string, string, string>>();
+      urlJsonParser.Create<TwitchStreamStatus.RootObject>(Arg.Any<string>(), Arg.Any<string>(), Arg.Any<string>()).Returns(_off, _on, _off, _off, _off, _on);
+      var container = _getContainer(timeService, urlJsonParser);
+      var streamStatusService = container.GetInstance<StreamStatusService>();
+
+      Assert.AreEqual(StreamStatus.Off, streamStatusService.GetStatus());
+      Assert.AreEqual(StreamStatus.On, streamStatusService.GetStatus());
+      Assert.AreEqual(StreamStatus.PossiblyOff, streamStatusService.GetStatus());
+      Assert.AreEqual(StreamStatus.PossiblyOff, streamStatusService.GetStatus());
+      Assert.AreEqual(StreamStatus.Off, streamStatusService.GetStatus());
+      Assert.AreEqual(StreamStatus.On, streamStatusService.GetStatus());
+
+      var unitOfWork = container.GetInstance<IQueryCommandService<IUnitOfWork>>();
+      var onTime = unitOfWork.Query(u => u.StateIntegers.LatestStreamOnTime);
+      Assert.AreEqual(_minu5, onTime);
+      var offTime = unitOfWork.Query(u => u.StateIntegers.LatestStreamOffTime);
+      Assert.AreEqual(_minu2, offTime);
+      var currentState = unitOfWork.Query(u => u.StateIntegers.StreamStatus);
+      Assert.AreEqual(StreamStatus.On, currentState);
+    }
+
+    [TestMethod]
+    public void StreamStatusService_OffOnOffOnMinuteLater_UpdatesDatabase() {
+      var timeService = Substitute.For<ITimeService>();
+      timeService.UtcNow.Returns(_time0, _minu1, _minu2, _minu3, _minu4, _minu5);
+      var urlJsonParser = Substitute.For<IGenericClassFactory<string, string, string>>();
+      urlJsonParser.Create<TwitchStreamStatus.RootObject>(Arg.Any<string>(), Arg.Any<string>(), Arg.Any<string>()).Returns(_off, _on, _off, _on);
+      var container = _getContainer(timeService, urlJsonParser);
+      var streamStatusService = container.GetInstance<StreamStatusService>();
+
+      Assert.AreEqual(StreamStatus.Off, streamStatusService.GetStatus());
+      Assert.AreEqual(StreamStatus.On, streamStatusService.GetStatus());
+      Assert.AreEqual(StreamStatus.PossiblyOff, streamStatusService.GetStatus());
+      Assert.AreEqual(StreamStatus.On, streamStatusService.GetStatus());
+
+      var unitOfWork = container.GetInstance<IQueryCommandService<IUnitOfWork>>();
+      var onTime = unitOfWork.Query(u => u.StateIntegers.LatestStreamOnTime);
+      Assert.AreEqual(_minu1, onTime);
+      var offTime = unitOfWork.Query(u => u.StateIntegers.LatestStreamOffTime);
+      Assert.AreEqual(_minu2, offTime);
+      var currentState = unitOfWork.Query(u => u.StateIntegers.StreamStatus);
+      Assert.AreEqual(StreamStatus.On, currentState);
+    }
+
+  }
+}
---
+++ Bot.Logic/StreamStatusService.cs
@@ -0,0 +1,58 @@
+﻿using Bot.Models.Json;
+using Bot.Repository.Interfaces;
+using Bot.Tools;
+using Bot.Tools.Interfaces;
+
+namespace Bot.Logic {
+  public class StreamStatusService {
+    private readonly IGenericClassFactory<string, string, string> _urlJsonParser;
+    private readonly IQueryCommandService<IUnitOfWork> _unitOfWork;
+    private readonly ITimeService _timeService;
+    private readonly ISettings _settings;
+
+    public StreamStatusService(
+      IGenericClassFactory<string, string, string> urlJsonParser,
+      IQueryCommandService<IUnitOfWork> unitOfWork,
+      ITimeService timeService,
+      ISettings settings) {
+      _urlJsonParser = urlJsonParser;
+      _unitOfWork = unitOfWork;
+      _timeService = timeService;
+      _settings = settings;
+    }
+
+    public StreamStatus GetStatus() {
+      var now = _timeService.UtcNow;
+      var streamStatus = _urlJsonParser.Create<TwitchStreamStatus.RootObject>("https://api.twitch.tv/kraken/streams/destiny", "", "");
+      var isOn = streamStatus.stream != null;
+      var previously = _unitOfWork.Query(u => u.StateIntegers.StreamStatus);
+      if (isOn) {
+        if (previously == StreamStatus.Off) {
+          _unitOfWork.Command(u => u.StateIntegers.LatestStreamOnTime = now);
+        }
+        if (previously != StreamStatus.On) {
+          _unitOfWork.Command(u => u.StateIntegers.StreamStatus = StreamStatus.On);
+        }
+        return StreamStatus.On;
+      }
+      // not on
+      if (previously == StreamStatus.On) {
+        _unitOfWork.Command(u => {
+          u.StateIntegers.StreamStatus = StreamStatus.PossiblyOff;
+          u.StateIntegers.LatestStreamOffTime = now;
+        });
+        return StreamStatus.PossiblyOff;
+      }
+      var possibleStreamOffTime = _unitOfWork.Query(u => u.StateIntegers.LatestStreamOffTime);
+      if (possibleStreamOffTime + _settings.OnOffTimeTolerance > now) {
+        return StreamStatus.PossiblyOff;
+      }
+      // outside tolerance
+      if (previously == StreamStatus.PossiblyOff) {
+        _unitOfWork.Command(u => u.StateIntegers.StreamStatus = StreamStatus.Off);
+      }
+      return StreamStatus.Off;
+    }
+
+  }
+}
