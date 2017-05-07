1e025656459070b0f32dffc57f96efa7b804a392
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-05-08 03:12:54 +1100

    Created a minimal DestinyGgParser

---
+++ Bot.Pipeline/DestinyGgParser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Bot.Models.Interfaces;
+using Bot.Models.Received;
+using Bot.Tools.Interfaces;
+
+namespace Bot.Pipeline {
+  public class DestinyGgParser : IErrorableFactory<string, IReceived<IUser, ITransmittable>> {
+
+    private readonly ITimeService _timeService;
+
+    public DestinyGgParser(ITimeService timeService) {
+      _timeService = timeService;
+    }
+
+    public IReceived<IUser, ITransmittable> Create(string input) {
+      throw new NotImplementedException();
+    }
+
+    public IReceived<IUser, ITransmittable> OnErrorCreate => new ReceivedError($"An error occured in {nameof(DestinyGgParser)}", _timeService);
+  }
+}
