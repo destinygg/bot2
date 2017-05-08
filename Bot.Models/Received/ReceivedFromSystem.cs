6d6ec01c24542c1e49f99d2b8c31610fa7504cd0
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-05-08 17:04:48 +1100

    Created ReceivedInitialUsers and ReceivedFromSystem

---
+++ Bot.Models/Received/ReceivedFromSystem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Bot.Models.Interfaces;
+
+namespace Bot.Models.Received {
+  public abstract class ReceivedFromSystem<TTransmission> : IReceived<Moderator, TTransmission>
+    where TTransmission : ITransmittable {
+    protected ReceivedFromSystem(DateTime timestamp) {
+      Timestamp = timestamp;
+    }
+
+    public DateTime Timestamp { get; }
+    public Moderator Sender => new Moderator("FROM_CLIENT_SYSTEM");
+    public abstract TTransmission Transmission { get; }
+    public abstract TResult Accept<TResult>(IReceivedVisitor<TResult> visitor);
+  }
+}
---
+++ Bot.Models/Received/ReceivedInitialUsers.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Bot.Models.Interfaces;
+
+namespace Bot.Models.Received {
+  public class ReceivedInitialUsers : ReceivedFromSystem<InitialUsers> {
+    public ReceivedInitialUsers(DateTime timestamp, InitialUsers initialUsers) : base(timestamp) {
+      Transmission = initialUsers;
+    }
+
+    public override InitialUsers Transmission { get; }
+    public override TResult Accept<TResult>(IReceivedVisitor<TResult> visitor) => visitor.Visit(this);
+  }
+}
