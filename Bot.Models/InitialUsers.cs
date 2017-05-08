4c7835531ee4c89dc6f3fd44e668cc3b070d36c8
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-05-08 16:18:19 +1100

    Created InitialUsers and InitialUsersSnapshot

---
+++ Bot.Models/InitialUsers.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Bot.Models.Interfaces;
+
+namespace Bot.Models {
+  public class InitialUsers : ITransmittable {
+    public InitialUsers(IEnumerable<IUser> users) {
+      Users = users.ToList(); // creates a new instance
+    }
+
+    public List<IUser> Users { get; }
+  }
+}
---
+++ Bot.Models/Snapshot/InitialUsersSnapshot.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using Bot.Models.Interfaces;
+
+namespace Bot.Models.Snapshot {
+  public class InitialUsersSnapshot : Snapshot<Moderator, InitialUsers> {
+    public InitialUsersSnapshot(IReceived<Moderator, InitialUsers> latest, IReadOnlyList<IReceived<IUser, ITransmittable>> context) : base(latest, context) { }
+    public override IReadOnlyList<ISendable<ITransmittable>> Accept(ISnapshotVisitor<IReadOnlyList<ISendable<ITransmittable>>> visitor) => visitor.Visit(this);
+  }
+}
