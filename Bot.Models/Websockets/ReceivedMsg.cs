023534fb51a7e25be8f1021dce4ae7f32686f788
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-05-08 20:38:19 -0500

    Created ReceivedMsg

---
+++ Bot.Models/Websockets/ReceivedMsg.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Bot.Models.Websockets {
+  public class ReceivedMsg {
+    public class RootObject {
+      public string nick { get; set; }
+      public List<string> features { get; set; }
+      public long timestamp { get; set; }
+      public string data { get; set; }
+    }
+  }
+}
