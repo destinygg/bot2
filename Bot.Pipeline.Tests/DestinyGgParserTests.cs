0920f6e94e02f31227f63e7d3bff31fb374ca654
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-05-08 19:28:14 -0500

    Implemented DestinyGgParser's parsing of InitialUsers and tested it.

---
+++ Bot.Pipeline.Tests/DestinyGgParserTests.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Bot.Models;
+using Bot.Tests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bot.Pipeline.Tests {
+  [TestClass]
+  public class DestinyGgParserTests {
+
+    [TestMethod]
+    public void InitialUsers_Parses_WithoutError() {
+      var data = TestData.DestinyGgClientNames;
+      var container = new TestContainerManager();
+      var parser = container.Container.GetInstance<DestinyGgParser>();
+      var received = parser.Create(data);
+    }
+
+    [TestMethod]
+    public void InitialUsers_ParsesIsPunishable_Properly() {
+      var data = TestData.DestinyGgClientNames;
+      var container = new TestContainerManager();
+      var parser = container.Container.GetInstance<DestinyGgParser>();
+      var received = parser.Create(data);
+      var initialUsers = (InitialUsers) received.Transmission;
+      var notPunishable = initialUsers.Users.Where(x => !x.IsPunishable).Select(x => x.Nick);
+      Assert.IsTrue(notPunishable.SequenceEqual(new List<string> { "RightToBearArmsLOL", "CeneZa", "Destiny", "Bot", "woopboop" }));
+    }
+
+    [TestMethod]
+    public void InitialUsers_ParsesMods_Properly() {
+      var data = TestData.DestinyGgClientNames;
+      var container = new TestContainerManager();
+      var parser = container.Container.GetInstance<DestinyGgParser>();
+      var received = parser.Create(data);
+      var initialUsers = (InitialUsers) received.Transmission;
+      var notPunishable = initialUsers.Users.Where(x => x.IsMod).Select(x => x.Nick);
+      Assert.IsTrue(notPunishable.SequenceEqual(new List<string> { "RightToBearArmsLOL", "CeneZa", "Destiny", "Bot" }));
+    }
+
+  }
+}
--- Bot.Pipeline/DestinyGgParser.cs
+++ Bot.Pipeline/DestinyGgParser.cs
@@ -1,19 +1,34 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Bot.Models;
 using Bot.Models.Interfaces;
 using Bot.Models.Received;
+using Bot.Models.Websockets;
 using Bot.Tools.Interfaces;
 
 namespace Bot.Pipeline {
   public class DestinyGgParser : IErrorableFactory<string, IReceived<IUser, ITransmittable>> {
 
+    private readonly IGenericClassFactory<string> _jsonParser;
     private readonly ITimeService _timeService;
 
-    public DestinyGgParser(ITimeService timeService) {
+    public DestinyGgParser(IGenericClassFactory<string> jsonParser, ITimeService timeService) {
+      _jsonParser = jsonParser;
       _timeService = timeService;
     }
 
     public IReceived<IUser, ITransmittable> Create(string input) {
-      throw new NotImplementedException();
+      var index = input.IndexOf(' ');
+      var command = input.Substring(0, index);
+      var json = input.Substring(index + 1);
+      if (command == "NAMES") {
+        var receivedNames = _jsonParser.Create<ReceivedNames.RootObject>(json);
+        var mods = receivedNames.users.Where(u => u.features.Any(f => f == "bot" || f == "admin" || f == "moderator")).Select(u => new Moderator(u.nick)).ToList();
+        var civilians = receivedNames.users.Where(u => !u.features.Any(f => f == "bot" || f == "admin" || f == "moderator")).Select(u => new Civilian(u.nick, u.features.All(f => f != "protected"))).ToList().OrderBy(x => x.Nick);
+        var initialUsers = new InitialUsers(new List<IUser>().Concat(mods).Concat(civilians));
+        return new ReceivedInitialUsers(_timeService.UtcNow, initialUsers);
+      }
+      return null;
     }
 
     public IReceived<IUser, ITransmittable> OnErrorCreate => new ReceivedError($"An error occured in {nameof(DestinyGgParser)}", _timeService);
