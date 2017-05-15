77997176dcc761a5f7157e7568899e8ff70bbb01
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-05-14 20:14:50 -0500

    IReceivedMessageExtensionMethods's StartsWith now takes a params of string args and is case insensitive

--- Bot.Models/Interfaces/IReceivedMessageExtensionMethods.cs
+++ Bot.Models/Interfaces/IReceivedMessageExtensionMethods.cs
@@ -1,8 +1,9 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Bot.Models.Interfaces {
   public static class IReceivedMessageExtensionMethods {
-    public static bool StartsWith(this IReceived<IUser, IMessage> message, string phrase) => message.Transmission.Text.StartsWith(phrase);
+    public static bool StartsWith(this IReceived<IUser, IMessage> message, params string[] phrases) => phrases.Any(p => message.Transmission.Text.StartsWith(p, System.StringComparison.InvariantCultureIgnoreCase));
     public static bool IsMatch(this IReceived<IUser, IMessage> message, Regex regex) => regex.IsMatch(message.Transmission.Text);
 
   }
