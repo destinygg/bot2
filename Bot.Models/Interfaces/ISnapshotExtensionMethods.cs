b999b3b1c9cc7cbef92c2f960923652d8906ad3d
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-05-17 08:11:15 -0500

    Created ISnapshotExtensionMethods which defines Sender

---
+++ Bot.Models/Interfaces/ISnapshotExtensionMethods.cs
@@ -0,0 +1,8 @@
+﻿namespace Bot.Models.Interfaces {
+  public static class ISnapshotExtensionMethods {
+    public static TUser Sender<TUser, TMessage>(this ISnapshot<TUser, TMessage> snapshot)
+      where TUser : IUser
+      where TMessage : ITransmittable =>
+      snapshot.Latest.Sender;
+  }
+}
