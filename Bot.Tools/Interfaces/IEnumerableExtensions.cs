3fe911262452d5ad1f688956cf93b2be5da80d9b
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-05-17 08:39:04 -0500

    Created IEnumerableExtensions and added ArgMax to it

---
+++ Bot.Tools/Interfaces/IEnumerableExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bot.Tools.Interfaces {
+  public static class IEnumerableExtensions {
+    public static TData ArgMax<TData, TColumn>(this IEnumerable<TData> set, Func<TData, TColumn> compareBy) =>
+      set.OrderByDescending(compareBy).FirstOrDefault();
+  }
+}
