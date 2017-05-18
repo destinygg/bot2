cb0e71eb7943c03978e53e871a555a48f83ec4e2
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-05-17 19:46:43 -0500

    Created SelfSpamBanFactory and tested it.

---
+++ Bot.Logic.Tests/SelfSpamBanFactoryTests.cs
@@ -0,0 +1,79 @@
+﻿using System.Linq;
+using Bot.Models;
+using Bot.Models.Interfaces;
+using Bot.Tests;
+using Bot.Tools.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bot.Logic.Tests {
+  [TestClass]
+  public class SelfSpamBanFactoryTests {
+
+    [TestMethod]
+    public void SelfSpamBanFactory_x1_DoesNotPunish() {
+      var nick = "nick";
+      var text = "text";
+      var container = new TestContainerManager().Container;
+      var snapshotFactory = container.GetInstance<IErrorableFactory<IReceived<IUser, ITransmittable>, ISnapshot<IUser, ITransmittable>>>();
+      var selfSpamBanFactory = container.GetInstance<SelfSpamBanFactory>();
+      var receivedFactory = container.GetInstance<ReceivedFactory>();
+      var snapshot = (ISnapshot<Civilian, PublicMessage>) snapshotFactory.Create(receivedFactory.PublicReceivedMessage(nick, text));
+
+      var bans = selfSpamBanFactory.Create(snapshot);
+
+      Assert.IsFalse(bans.Any());
+    }
+
+    [TestMethod]
+    public void SelfSpamBanFactory_x2_DoesNotPunish() {
+      var nick = "nick";
+      var text = "text";
+      var container = new TestContainerManager().Container;
+      var snapshotFactory = container.GetInstance<IErrorableFactory<IReceived<IUser, ITransmittable>, ISnapshot<IUser, ITransmittable>>>();
+      var selfSpamBanFactory = container.GetInstance<SelfSpamBanFactory>();
+      var receivedFactory = container.GetInstance<ReceivedFactory>();
+      snapshotFactory.Create(receivedFactory.PublicReceivedMessage(nick, text));
+      var snapshot = (ISnapshot<Civilian, PublicMessage>) snapshotFactory.Create(receivedFactory.PublicReceivedMessage(nick, text));
+
+      var bans = selfSpamBanFactory.Create(snapshot);
+
+      Assert.IsFalse(bans.Any());
+    }
+
+    [TestMethod]
+    public void SelfSpamBanFactory_x3_Punishes() {
+      var nick = "nick";
+      var text = "text";
+      var container = new TestContainerManager().Container;
+      var snapshotFactory = container.GetInstance<IErrorableFactory<IReceived<IUser, ITransmittable>, ISnapshot<IUser, ITransmittable>>>();
+      var selfSpamBanFactory = container.GetInstance<SelfSpamBanFactory>();
+      var receivedFactory = container.GetInstance<ReceivedFactory>();
+      snapshotFactory.Create(receivedFactory.PublicReceivedMessage(nick, text));
+      snapshotFactory.Create(receivedFactory.PublicReceivedMessage(nick, text));
+      var snapshot = (ISnapshot<Civilian, PublicMessage>) snapshotFactory.Create(receivedFactory.PublicReceivedMessage(nick, text));
+
+      var bans = selfSpamBanFactory.Create(snapshot);
+
+      Assert.IsTrue(bans.Any());
+    }
+
+    [TestMethod]
+    public void SelfSpamBanFactory_x4_Punishes() {
+      var nick = "nick";
+      var text = "text";
+      var container = new TestContainerManager().Container;
+      var snapshotFactory = container.GetInstance<IErrorableFactory<IReceived<IUser, ITransmittable>, ISnapshot<IUser, ITransmittable>>>();
+      var selfSpamBanFactory = container.GetInstance<SelfSpamBanFactory>();
+      var receivedFactory = container.GetInstance<ReceivedFactory>();
+      snapshotFactory.Create(receivedFactory.PublicReceivedMessage(nick, text));
+      snapshotFactory.Create(receivedFactory.PublicReceivedMessage(nick, text));
+      snapshotFactory.Create(receivedFactory.PublicReceivedMessage(nick, text));
+      var snapshot = (ISnapshot<Civilian, PublicMessage>) snapshotFactory.Create(receivedFactory.PublicReceivedMessage(nick, text));
+
+      var bans = selfSpamBanFactory.Create(snapshot);
+
+      Assert.IsTrue(bans.Any());
+    }
+
+  }
+}
---
+++ Bot.Logic/SelfSpamBanFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Bot.Models;
+using Bot.Models.Interfaces;
+using Bot.Models.Sendable;
+using Bot.Tools;
+using Bot.Tools.Interfaces;
+
+namespace Bot.Logic {
+  public class SelfSpamBanFactory : IFactory<ISnapshot<Civilian, PublicMessage>, IReadOnlyList<ISendable<ITransmittable>>> {
+    private readonly ITimeService _timeService;
+    private readonly ISettings _settings;
+
+    public SelfSpamBanFactory(ITimeService timeService, ISettings settings) {
+      _timeService = timeService;
+      _settings = settings;
+    }
+
+    public IReadOnlyList<ISendable<ITransmittable>> Create(ISnapshot<Civilian, PublicMessage> snapshot) {
+      var similarValues = snapshot.Context
+        .OfType<IReceived<Civilian, PublicMessage>>()
+        .Where(r => r.Timestamp + _settings.SelfSpamWindow > _timeService.UtcNow)
+        .Where(r => r.Sender.Nick == snapshot.Latest.Sender.Nick)
+        .Select(r => r.Transmission.Text.SimilarTo(snapshot.Latest.Transmission.Text)).ToList();
+      var similarCount = similarValues.Count(p => p > _settings.MinimumPunishmentSimilarity);
+      return similarCount < 2
+        ? (IReadOnlyList<ISendable<ITransmittable>>) new List<ISendable<ITransmittable>>()
+        : new SendableMute(snapshot.Sender(), TimeSpan.FromMinutes(2), $"2m {snapshot.Sender().Nick}: {similarValues.Average()}% = your past text").Wrap().ToList();
+    }
+
+  }
+}
