548d79f1474d5fee81cb54a1435052deb87db259
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-05-18 19:27:49 -0500

    Created DestinyGgSendingClient

--- Bot.Pipeline/DestinyGgBaseClient.cs
+++ Bot.Pipeline/DestinyGgBaseClient.cs
@@ -11,31 +11,32 @@ namespace Bot.Pipeline {
     private const int MaximumBackoffTimeInSeconds = 60;
     private readonly ILogger _logger;
     private readonly ITimeService _timeService;
-    private readonly WebSocket _websocket;
     private DateTime _lastConnectedAt;
     private int _connectionAttemptedCount;
+
+    protected readonly WebSocket Websocket;
 
     public DestinyGgBaseClient(IPrivateConstants privateConstants, ILogger logger, ITimeService timeService) {
       _logger = logger;
       _timeService = timeService;
-      _websocket = new WebSocket("ws://www.destiny.gg:9998/ws");
-      _websocket.SetCookie(new Cookie("authtoken", privateConstants.BotWebsocketAuth));
-      _websocket.OnMessage += WebsocketMessaged;
-      _websocket.OnClose += WebsocketClosed;
-      _websocket.OnError += WebsocketErrored;
-      _websocket.OnOpen += WebsocketOpened;
+      Websocket = new WebSocket("ws://www.destiny.gg:9998/ws");
+      Websocket.SetCookie(new Cookie("authtoken", privateConstants.BotWebsocketAuth));
+      Websocket.OnMessage += WebsocketMessaged;
+      Websocket.OnClose += WebsocketClosed;
+      Websocket.OnError += WebsocketErrored;
+      Websocket.OnOpen += WebsocketOpened;
     }
 
     public void Connect() {
       if (_lastConnectedAt - _timeService.UtcNow > TimeSpan.FromSeconds(MaximumBackoffTimeInSeconds)) {
         _connectionAttemptedCount = 0;
       }
-      while (_websocket.ReadyState != WebSocketState.Open) {
+      while (Websocket.ReadyState != WebSocketState.Open) {
         try {
           var backoffTimeInSeconds = Math.Min((int) Math.Pow(2, _connectionAttemptedCount) - 1, MaximumBackoffTimeInSeconds);
           Thread.Sleep(TimeSpan.FromSeconds(backoffTimeInSeconds));
           _logger.LogInformation($"Connecting... {nameof(backoffTimeInSeconds)} is {backoffTimeInSeconds}. {nameof(_connectionAttemptedCount)} is {_connectionAttemptedCount}.");
-          _websocket.Connect();
+          Websocket.Connect();
           _connectionAttemptedCount++;
         } catch (Exception e) {
           _logger.LogError($"{nameof(DestinyGgBaseClient)} had an error connecting.", e);
---
+++ Bot.Pipeline/DestinyGgSendingClient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Bot.Tools.Interfaces;
+using Bot.Tools.Logging;
+
+namespace Bot.Pipeline {
+  public class DestinyGgSendingClient : DestinyGgBaseClient {
+    private readonly ILogger _logger;
+    private Action<string> _receiveAction;
+
+    public DestinyGgSendingClient(IPrivateConstants privateConstants, ILogger logger, ITimeService timeService) : base(privateConstants, logger, timeService) {
+      _logger = logger;
+    }
+
+    public override void Receive(string input) {
+      _receiveAction(input);
+      _logger.LogInformation(input);
+    }
+
+    public override void Send(string data) => Websocket.Send(data);
+
+    public override void SetReceive(Action<string> receiveAction) => _receiveAction = receiveAction;
+
+  }
+}
