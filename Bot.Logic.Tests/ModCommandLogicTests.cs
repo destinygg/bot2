e3454bf2fc9e6185c6143aa87fd3f256a17e4b33
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-05-19 08:47:17 -0500

    Added Stalk to IModCommandLogic and tested it

---
+++ Bot.Logic.Tests/ModCommandLogicTests.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Bot.Logic.Interfaces;
+using Bot.Models.Sendable;
+using Bot.Tests;
+using Bot.Tools;
+using Bot.Tools.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using SimpleInjector;
+
+namespace Bot.Logic.Tests {
+  [TestClass]
+  public class ModCommandLogicTests {
+    private TestContainerManager CreateTestContainerManager(string data) {
+      var downloadFactory = Substitute.For<IFactory<string, string, string>>();
+      downloadFactory.Create(Arg.Any<string>(), Arg.Any<string>()).Returns(data);
+      var timeService = Substitute.For<ITimeService>();
+      timeService.UtcNow.Returns(new DateTime(2017, 1, 2, 0, 0, 0));
+      return new TestContainerManager(c => {
+        c.RegisterConditional<IGenericClassFactory<string, string, string>, UrlJsonParser>(Lifestyle.Singleton, _ => true);
+        var downloadFactoryRegistration = Lifestyle.Singleton.CreateRegistration(() => downloadFactory, c);
+        c.RegisterConditional(typeof(IFactory<string, string, string>), downloadFactoryRegistration, _ => true);
+        var timeServiceRegistration = Lifestyle.Singleton.CreateRegistration(() => timeService, c);
+        c.RegisterConditional(typeof(ITimeService), timeServiceRegistration, pc => !pc.Handled);
+      }, s => s.ClientType = "DestinyGg");
+    }
+
+    [TestMethod]
+    public void ModCommandLogic_OverRustleLogs_Returns3Lines() {
+      var data = TestData.OverrustleLogs4Lines;
+      var testContainerManager = CreateTestContainerManager(data).Container;
+      var modCommandLogic = testContainerManager.GetInstance<IModCommandLogic>();
+      var expected = new List<string> {
+        "dgg.overrustlelogs.net/woopboop",
+        "23h 58m ago: Some words 2017-01-01 00:02:00 UTC third to last",
+        "23h ago: Some words 2017-01-01 01:00:00 UTC penultimate",
+        "22h ago: Some words 2017-01-01 02:00:00 UTC last"
+      };
+
+      var logs = modCommandLogic.Stalk("woopboop");
+
+      var text = logs.Cast<SendablePublicMessage>().Select(l => l.Transmission.Text);
+      Assert.IsTrue(expected.SequenceEqual(text));
+    }
+
+    [TestMethod]
+    public void ModCommandLogic_OverRustleLogsTwoLogs_Returns2Lines() {
+      var data = TestData.OverrustleLogs2Lines;
+      var testContainerManager = CreateTestContainerManager(data).Container;
+      var modCommandLogic = testContainerManager.GetInstance<IModCommandLogic>();
+      var expected = new List<string> {
+        "dgg.overrustlelogs.net/woopboop",
+        "23h ago: Some words 2017-01-01 01:00:00 UTC penultimate",
+        "22h ago: Some words 2017-01-01 02:00:00 UTC last"
+      };
+
+      var logs = modCommandLogic.Stalk("woopboop");
+
+      var text = logs.Cast<SendablePublicMessage>().Select(l => l.Transmission.Text);
+      Assert.IsTrue(expected.SequenceEqual(text));
+    }
+
+    [TestMethod]
+    public void ModCommandLogic_OverRustleLogs1Log_Returns1Line() {
+      var data = TestData.OverrustleLogs1Line;
+      var testContainerManager = CreateTestContainerManager(data).Container;
+      var modCommandLogic = testContainerManager.GetInstance<IModCommandLogic>();
+      var expected = new List<string> {
+        "dgg.overrustlelogs.net/woopboop",
+        "22h ago: Some words 2017-01-01 02:00:00 UTC last"
+      };
+
+      var logs = modCommandLogic.Stalk("woopboop");
+
+      var text = logs.Cast<SendablePublicMessage>().Select(l => l.Transmission.Text);
+      Console.WriteLine(ObjectDumper.Dump(text));
+      Assert.IsTrue(expected.SequenceEqual(text));
+    }
+
+    [TestMethod]
+    public void ModCommandLogic_OverRustleLogs404_ReturnsUserNotFound_DoNotRunContinuously() {
+      var user = TestHelper.RandomString();
+      var testContainerManager = new TestContainerManager(configureSettings: s => s.ClientType = "DestinyGg");
+      var modCommandLogic = testContainerManager.Container.GetInstance<IModCommandLogic>();
+      var expected = new List<string> {
+        $"{user} not found",
+      };
+
+      var logs = modCommandLogic.Stalk(user);
+
+      var text = logs.Cast<SendablePublicMessage>().Select(l => l.Transmission.Text);
+      Console.WriteLine(ObjectDumper.Dump(text));
+      Assert.IsTrue(expected.SequenceEqual(text));
+    }
+
+  }
+}
--- Bot.Logic/ModCommandLogic.cs
+++ Bot.Logic/ModCommandLogic.cs
@@ -1,5 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
 using Bot.Logic.Interfaces;
 using Bot.Models;
 using Bot.Models.Interfaces;
@@ -11,20 +15,29 @@ using Bot.Tools.Logging;
 
 namespace Bot.Logic {
   public class ModCommandLogic : IModCommandLogic {
-    private readonly ILogger _logger;
     private readonly IErrorableFactory<Nuke, IReadOnlyList<IReceived<IUser, ITransmittable>>, IReadOnlyList<ISendable<ITransmittable>>> _nukeMuteFactory;
     private readonly IErrorableFactory<IReadOnlyList<IReceived<IUser, ITransmittable>>, IReadOnlyList<ISendable<ITransmittable>>> _aegisPardonFactory;
     private readonly IQueryCommandService<IUnitOfWork> _repository;
+    private readonly IDownloadMapper _downloadMapper;
+    private readonly ITimeService _timeService;
+    private readonly ISettings _settings;
+    private readonly ILogger _logger;
 
     public ModCommandLogic(
       IErrorableFactory<Nuke, IReadOnlyList<IReceived<IUser, ITransmittable>>, IReadOnlyList<ISendable<ITransmittable>>> nukeMuteFactory,
       IErrorableFactory<IReadOnlyList<IReceived<IUser, ITransmittable>>, IReadOnlyList<ISendable<ITransmittable>>> aegisPardonFactory,
       IQueryCommandService<IUnitOfWork> repository,
+      IDownloadMapper downloadMapper,
+      ITimeService timeService,
+      ISettings settings,
       ILogger logger) {
-      _logger = logger;
       _nukeMuteFactory = nukeMuteFactory;
       _aegisPardonFactory = aegisPardonFactory;
       _repository = repository;
+      _downloadMapper = downloadMapper;
+      _timeService = timeService;
+      _settings = settings;
+      _logger = logger;
     }
 
     public ISendable<PublicMessage> Long(IReadOnlyList<IReceived<IUser, ITransmittable>> context) {
@@ -77,6 +90,33 @@ namespace Bot.Logic {
         confirmation = $"!{command} removed";
       }
       return new SendablePublicMessage(confirmation).Wrap().ToList();
+    }
+
+    public IReadOnlyList<ISendable<ITransmittable>> Stalk(string user) {
+      var output = new List<ISendable<ITransmittable>>();
+      if (_settings.ClientType.Contains("DestinyGg")) {
+        output.Add(new SendablePublicMessage($"dgg.overrustlelogs.net/{user}"));
+        string rawDownload;
+        try {
+          rawDownload = _downloadMapper.OverRustleLogs(user);
+        } catch (WebException wex) {
+          if (((HttpWebResponse) wex.Response).StatusCode == HttpStatusCode.NotFound) {
+            return new SendablePublicMessage($"{user} not found").Wrap().ToList();
+          }
+          throw;
+        }
+        var lines = Regex.Split(rawDownload, "\n");
+        var lastlines = lines.TakeLast(4).Where(l => !string.IsNullOrWhiteSpace(l)); // last line is empty
+        foreach (var line in lastlines) {
+          var match = new Regex(@"\[([\d- :]+)UTC] (?:.*: )(.*)").Match(line);
+          var stringTime = match.Groups[1].Value;
+          var message = match.Groups[2].Value;
+          var timeStamp = DateTime.ParseExact(stringTime, "yyyy-MM-dd HH:mm:ss ", CultureInfo.InvariantCulture);
+          var delta = (_timeService.UtcNow - timeStamp).ToPretty(_logger);
+          output.Add(new SendablePublicMessage($"{delta} ago: {message}"));
+        }
+      }
+      return output;
     }
 
   }
