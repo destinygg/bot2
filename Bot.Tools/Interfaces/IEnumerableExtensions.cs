7b0b1f30fb964aa88535c26cf052b197b7c5a9e2
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-05-19 08:26:25 -0500

    Added TakeLast to IEnumerableExtensions

--- Bot.Tools/Interfaces/IEnumerableExtensions.cs
+++ Bot.Tools/Interfaces/IEnumerableExtensions.cs
@@ -6,5 +6,11 @@ namespace Bot.Tools.Interfaces {
   public static class IEnumerableExtensions {
     public static TData ArgMax<TData, TColumn>(this IEnumerable<TData> set, Func<TData, TColumn> compareBy) =>
       set.OrderByDescending(compareBy).FirstOrDefault();
+
+    public static IEnumerable<T> TakeLast<T>(this IEnumerable<T> source, int n) {
+      var enumerated = source.ToList();
+      return enumerated.Skip(Math.Max(0, enumerated.Count - n));
+    }
+
   }
 }
