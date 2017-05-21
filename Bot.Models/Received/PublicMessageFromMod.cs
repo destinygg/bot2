07949e73529f13986a97fe7c403ea7e27739e05a
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-05-21 04:13:54 -0500

    Added a new ctor to PublicMessageFromMod

--- Bot.Models/Received/PublicMessageFromMod.cs
+++ Bot.Models/Received/PublicMessageFromMod.cs
@@ -8,6 +8,9 @@ namespace Bot.Models.Received {
     public PublicMessageFromMod(string text, ITimeService timeService)
       : base(new Moderator("SampleMod"), text, timeService) { }
 
+    public PublicMessageFromMod(string nick, string text, ITimeService timeService)
+      : base(new Moderator(nick), text, timeService) { }
+
     public PublicMessageFromMod(string text, DateTime timestamp)
       : base(new Moderator(timestamp.ToShortTimeString()), text, timestamp) { }
 
