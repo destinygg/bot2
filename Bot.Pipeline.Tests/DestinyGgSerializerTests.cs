f1bdf569a1709cdb3d0a60a4f8324ee9011bce8c
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-05-21 11:04:39 -0500

    Added logic to DestinyGgSerializer to ensure that adjacent messages are never exactly the same. Tested it

--- Bot.Pipeline.Tests/DestinyGgSerializerTests.cs
+++ Bot.Pipeline.Tests/DestinyGgSerializerTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Bot.Models;
 using Bot.Models.Sendable;
@@ -56,6 +57,118 @@ namespace Bot.Pipeline.Tests {
       var serialized = destinyGgSerializer.Create(sendableBan.Wrap().ToList());
 
       Assert.AreEqual(expected, serialized.Single());
+    }
+
+    [TestMethod]
+    public void DestinyGgSerializer_SingleMessage_IsNotModified() {
+      var text = "and here, we... go";
+      var container = new TestContainerManager().Container;
+      var message = new SendablePublicMessage(text);
+      var destinyGgSerializer = container.GetInstance<DestinyGgSerializer>();
+      var expected = @"MSG {""Data"":""and here, we... go""}";
+
+      var serialized = destinyGgSerializer.Create(message.Wrap().ToList());
+
+      Assert.AreEqual(expected, serialized.Single());
+    }
+
+    [TestMethod]
+    public void DestinyGgSerializer_TwoRepeatedMessages_AreMadeDistinct() {
+      var text = "and here, we... go";
+      var message = new SendablePublicMessage(text);
+      var messages = new List<SendablePublicMessage> { message, message };
+      var container = new TestContainerManager().Container;
+      var destinyGgSerializer = container.GetInstance<DestinyGgSerializer>();
+      var expected1 = @"MSG {""Data"":""and here, we... go""}";
+      var expected2 = @"MSG {""Data"":""and here, we... go.""}";
+
+      var serialized = destinyGgSerializer.Create(messages).ToList();
+
+      Assert.AreEqual(2, serialized.Count);
+      Assert.AreEqual(expected1, serialized.First());
+      Assert.AreEqual(expected2, serialized.Skip(1).First());
+    }
+
+    [TestMethod]
+    public void DestinyGgSerializer_TwoCallsSameMessage_AreMadeDistinct() {
+      var text = "and here, we... go";
+      var message = new SendablePublicMessage(text);
+      var messages = new List<SendablePublicMessage> { message };
+      var container = new TestContainerManager().Container;
+      var destinyGgSerializer = container.GetInstance<DestinyGgSerializer>();
+      var expected1 = @"MSG {""Data"":""and here, we... go""}";
+      var expected2 = @"MSG {""Data"":""and here, we... go.""}";
+
+      var serialized1 = destinyGgSerializer.Create(messages).ToList();
+      var serialized2 = destinyGgSerializer.Create(messages).ToList();
+
+      Assert.AreEqual(1, serialized1.Count);
+      Assert.AreEqual(1, serialized2.Count);
+      Assert.AreEqual(expected1, serialized1.Single());
+      Assert.AreEqual(expected2, serialized2.Single());
+    }
+
+    [TestMethod]
+    public void DestinyGgSerializer_ThreeRepeatedMessages_AreMadeAlternating() {
+      var text = "and here, we... go";
+      var message = new SendablePublicMessage(text);
+      var messages = new List<SendablePublicMessage> { message, message, message };
+      var container = new TestContainerManager().Container;
+      var destinyGgSerializer = container.GetInstance<DestinyGgSerializer>();
+      var expected1 = @"MSG {""Data"":""and here, we... go""}";
+      var expected2 = @"MSG {""Data"":""and here, we... go.""}";
+      var expected3 = @"MSG {""Data"":""and here, we... go""}";
+
+      var serialized = destinyGgSerializer.Create(messages).ToList();
+
+      Assert.AreEqual(3, serialized.Count);
+      Assert.AreEqual(expected1, serialized.Skip(0).First());
+      Assert.AreEqual(expected2, serialized.Skip(1).First());
+      Assert.AreEqual(expected3, serialized.Skip(2).First());
+    }
+
+    [TestMethod]
+    public void DestinyGgSerializer_ThreeSeparateRepeatedMessages_AreMadeAlternating() {
+      var text = "and here, we... go";
+      var message = new SendablePublicMessage(text);
+      var messages = new List<SendablePublicMessage> { message };
+      var container = new TestContainerManager().Container;
+      var destinyGgSerializer = container.GetInstance<DestinyGgSerializer>();
+      var expected1 = @"MSG {""Data"":""and here, we... go""}";
+      var expected2 = @"MSG {""Data"":""and here, we... go.""}";
+      var expected3 = @"MSG {""Data"":""and here, we... go""}";
+
+      var serialized1 = destinyGgSerializer.Create(messages).ToList();
+      var serialized2 = destinyGgSerializer.Create(messages).ToList();
+      var serialized3 = destinyGgSerializer.Create(messages).ToList();
+
+      Assert.AreEqual(1, serialized1.Count);
+      Assert.AreEqual(1, serialized2.Count);
+      Assert.AreEqual(1, serialized3.Count);
+      Assert.AreEqual(expected1, serialized1.Single());
+      Assert.AreEqual(expected2, serialized2.Single());
+      Assert.AreEqual(expected3, serialized3.Single());
+    }
+
+    [TestMethod]
+    public void DestinyGgSerializer_FourRepeatedMessages_AreMadeAlternating() {
+      var text = "and here, we... go";
+      var message = new SendablePublicMessage(text);
+      var messages = new List<SendablePublicMessage> { message, message, message, message };
+      var container = new TestContainerManager().Container;
+      var destinyGgSerializer = container.GetInstance<DestinyGgSerializer>();
+      var expected1 = @"MSG {""Data"":""and here, we... go""}";
+      var expected2 = @"MSG {""Data"":""and here, we... go.""}";
+      var expected3 = @"MSG {""Data"":""and here, we... go""}";
+      var expected4 = @"MSG {""Data"":""and here, we... go.""}";
+
+      var serialized = destinyGgSerializer.Create(messages).ToList();
+
+      Assert.AreEqual(4, serialized.Count);
+      Assert.AreEqual(expected1, serialized.Skip(0).First());
+      Assert.AreEqual(expected2, serialized.Skip(1).First());
+      Assert.AreEqual(expected3, serialized.Skip(2).First());
+      Assert.AreEqual(expected4, serialized.Skip(3).First());
     }
 
   }
--- Bot.Pipeline/DestinyGgSerializer.cs
+++ Bot.Pipeline/DestinyGgSerializer.cs
@@ -1,14 +1,30 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using Bot.Models.Interfaces;
+using Bot.Models.Sendable;
 using Bot.Tools.Interfaces;
 using Newtonsoft.Json;
 
 namespace Bot.Pipeline {
   public class DestinyGgSerializer : IFactory<IEnumerable<ISendable<ITransmittable>>, IEnumerable<string>> {
 
-    public IEnumerable<string> Create(IEnumerable<ISendable<ITransmittable>> input) =>
-      input.Select(s => $"{s.Json.Command} {JsonConvert.SerializeObject(s.Json)}");
+    private SendablePublicMessage _cache = new SendablePublicMessage("");
+
+    public IEnumerable<string> Create(IEnumerable<ISendable<ITransmittable>> sendables) {
+      var r = new List<ISendable<ITransmittable>>();
+      sendables = sendables.ToList();
+      foreach (var sendable in sendables) {
+        if (sendable is SendablePublicMessage) {
+          var sendablePublicMessage = (SendablePublicMessage) sendable;
+          var toAdd = sendablePublicMessage.Text == _cache.Text ? new SendablePublicMessage(sendablePublicMessage.Text + ".") : sendablePublicMessage;
+          r.Add(toAdd);
+          _cache = toAdd;
+        } else {
+          r.Add(sendable);
+        }
+      }
+      return r.Select(s => $"{s.Json.Command} {JsonConvert.SerializeObject(s.Json)}");
+    }
 
   }
 }
