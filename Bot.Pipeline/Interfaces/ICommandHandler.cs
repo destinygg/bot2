f5342898054f57b0b5276ec73b2164facce36580
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-06-04 11:19:22 -0500

    Created a ICommandHandler which takes no arguments

--- Bot.Pipeline/Interfaces/ICommandHandler.cs
+++ Bot.Pipeline/Interfaces/ICommandHandler.cs
@@ -1,5 +1,11 @@
 ﻿namespace Bot.Pipeline.Interfaces {
+
+  public interface ICommandHandler {
+    void Handle();
+  }
+
   public interface ICommandHandler<in TCommand> {
     void Handle(TCommand command);
   }
+
 }
