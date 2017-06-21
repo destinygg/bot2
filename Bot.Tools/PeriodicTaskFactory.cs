17417008e31eb17be2be1daa6fd1b4fd94350339
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-06-20 20:46:10 -0500

    Marked PeriodicTaskFactory's Create with async/await to make semantics clearer

--- Bot.Tools/PeriodicTaskFactory.cs
+++ Bot.Tools/PeriodicTaskFactory.cs
@@ -6,14 +6,15 @@ using Bot.Tools.Interfaces;
 namespace Bot.Tools {
   public class PeriodicTaskFactory : IFactory<TimeSpan, Action, Task> {
 
-    public Task Create(TimeSpan period, Action action) =>
-      Run(action, period, CancellationToken.None);
+    public async Task Create(TimeSpan period, Action action) =>
+      await Run(action, period, CancellationToken.None);
 
     private async Task Run(Action action, TimeSpan period, CancellationToken cancellationToken) {
       while (!cancellationToken.IsCancellationRequested) {
         action();
         await Task.Delay(period, cancellationToken);
       }
+
     }
 
   }
