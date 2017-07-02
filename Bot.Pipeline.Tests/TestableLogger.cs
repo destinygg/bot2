ce0cc562fd566ce97e95e953dd0258966d165038
DharmaTurtle@gmail.com <dharmaturtle@gmail.com>
2017-07-02 15:06:45 -0500

    Updated TestableLogger to remember what level logs occur at and tested it

--- Bot.Pipeline.Tests/TestableLogger.cs
+++ Bot.Pipeline.Tests/TestableLogger.cs
@@ -1,38 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Bot.Tools.Logging;
 
 namespace Bot.Pipeline.Tests {
   public class TestableLogger : ILogger {
 
     public void LogDebug(string message) {
-      Outbox.Add(message);
+      LeveledOutbox.Add(Tuple.Create(message, Level.Debug));
     }
 
     public void LogInformation(string message) {
-      Outbox.Add(message);
+      LeveledOutbox.Add(Tuple.Create(message, Level.Information));
     }
 
     public void LogWarning(string message) {
-      Outbox.Add(message);
+      LeveledOutbox.Add(Tuple.Create(message, Level.Warning));
     }
 
     public void LogError(string message) {
-      Outbox.Add(message);
+      LeveledOutbox.Add(Tuple.Create(message, Level.Error));
     }
 
     public void LogError(string message, Exception exception) {
-      Outbox.Add(message);
+      LeveledOutbox.Add(Tuple.Create(message, Level.Error));
     }
 
     public void LogFatal(string message) {
-      Outbox.Add(message);
+      LeveledOutbox.Add(Tuple.Create(message, Level.Fatal));
     }
 
     public void LogFatal(string message, Exception exception) {
-      Outbox.Add(message);
+      LeveledOutbox.Add(Tuple.Create(message, Level.Fatal));
     }
 
-    public IList<string> Outbox { get; } = new List<string>();
+    public IList<string> DebugOutbox => LeveledOutbox.Where(x => x.Item2 >= Level.Debug).Select(x => x.Item1).ToList();
+
+    public IList<string> InformationOutbox => LeveledOutbox.Where(x => x.Item2 >= Level.Information).Select(x => x.Item1).ToList();
+
+    public IList<string> WarningOutbox => LeveledOutbox.Where(x => x.Item2 >= Level.Warning).Select(x => x.Item1).ToList();
+
+    public IList<string> ErrorOutbox => LeveledOutbox.Where(x => x.Item2 >= Level.Error).Select(x => x.Item1).ToList();
+
+    public IList<string> FatalOutbox => LeveledOutbox.Where(x => x.Item2 >= Level.Fatal).Select(x => x.Item1).ToList();
+
+    public IList<string> Outbox => LeveledOutbox.Select(x => x.Item1).ToList();
+
+    public IList<Tuple<string, Level>> LeveledOutbox { get; } = new List<Tuple<string, Level>>();
+
+    public enum Level { Debug, Information, Warning, Error, Fatal }
   }
 }
---
+++ Bot.Pipeline.Tests/TestableLoggerTests.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bot.Pipeline.Tests {
+  [TestClass]
+  public class TestableLoggerTests {
+
+    private const string Debug = "Debug";
+    private const string Information = "Information";
+    private const string Warning = "Warning";
+    private const string Error = "Error";
+    private const string Fatal = "Fatal";
+
+    private static TestableLogger _createFullyPopulatedTestableLogger() {
+      var testableLogger = new TestableLogger();
+      testableLogger.LogDebug(Debug);
+      testableLogger.LogInformation(Information);
+      testableLogger.LogWarning(Warning);
+      testableLogger.LogError(Error);
+      testableLogger.LogFatal(Fatal);
+      return testableLogger;
+    }
+
+    [TestMethod]
+    public void TestableLogger_DebugOutbox_ReturnsDebugAndLower() {
+      var testableLogger = _createFullyPopulatedTestableLogger();
+
+      var debugOutbox = testableLogger.DebugOutbox;
+
+      Assert.IsTrue(debugOutbox.SequenceEqual(new List<string> { Debug, Information, Warning, Error, Fatal }));
+    }
+
+    [TestMethod]
+    public void TestableLogger_InformationOutbox_ReturnsInformationAndLower() {
+      var testableLogger = _createFullyPopulatedTestableLogger();
+
+      var debugOutbox = testableLogger.InformationOutbox;
+
+      Assert.IsTrue(debugOutbox.SequenceEqual(new List<string> { Information, Warning, Error, Fatal }));
+    }
+
+    [TestMethod]
+    public void TestableLogger_WarningOutbox_ReturnsWarningAndLower() {
+      var testableLogger = _createFullyPopulatedTestableLogger();
+
+      var debugOutbox = testableLogger.WarningOutbox;
+
+      Assert.IsTrue(debugOutbox.SequenceEqual(new List<string> { Warning, Error, Fatal }));
+    }
+
+    [TestMethod]
+    public void TestableLogger_ErrorOutbox_ReturnsErrorAndLower() {
+      var testableLogger = _createFullyPopulatedTestableLogger();
+
+      var debugOutbox = testableLogger.ErrorOutbox;
+
+      Assert.IsTrue(debugOutbox.SequenceEqual(new List<string> { Error, Fatal }));
+    }
+
+    [TestMethod]
+    public void TestableLogger_FatalOutbox_ReturnsFatalAndLower() {
+      var testableLogger = _createFullyPopulatedTestableLogger();
+
+      var debugOutbox = testableLogger.FatalOutbox;
+
+      Assert.IsTrue(debugOutbox.SequenceEqual(new List<string> { Fatal }));
+    }
+
+  }
+}
